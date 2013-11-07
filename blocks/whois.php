6c19f5ede9f8e01717456f3826fb300e90bfd332
blitze <blitze@vm-5.blitze.koding.kd.io>
2013-11-07 04:55:06 +0000

    No need to reinvent the wheel

--- blocks/whois.php
+++ blocks/whois.php
@@ -40,37 +40,23 @@ class whois extends \primetime\primetime\core\blocks\driver\block
 	* @param \phpbb\config\config	$config		phpBB configuration
 	* @param \phpbb\user			$user       User object
 	*/
-	public function __construct(\phpbb\config\config $config, \phpbb\user $user)
+	public function __construct(\phpbb\config\config $config, \phpbb\user $user, \phpbb\template\context $context)
 	{
 		$this->config = $config;
 		$this->user = $user;
+		$this->context = $context;
 	}
 
 	public function display($settings, $edit_mode = false)
 	{
-		$item = 'forum';
-		$item_id = 0;
-
-		$online_users = obtain_users_online($item_id, $item);
-		$user_online_strings = obtain_users_online_string($online_users, $item_id, $item);
-
-		$l_online_users = $user_online_strings['l_online_users'];
-		$online_userlist = $user_online_strings['online_userlist'];
-		$total_online_users = $online_users['total_online'];
-
-		if ($total_online_users > $this->config['record_online_users'])
-		{
-			set_config('record_online_users', $total_online_users, true);
-			set_config('record_online_date', time(), true);
-		}
-
-		$l_online_record = $this->user->lang('RECORD_ONLINE_USERS', (int) $this->config['record_online_users'], $this->user->format_date($this->config['record_online_date'], false, true));
+		$data = $this->context->get_data_ref();
 
 		$this->btemplate->assign_vars(array(
-			'TOTAL_USERS_ONLINE'	=> $l_online_users,
-			'LOGGED_IN_USER_LIST'	=> $online_userlist,
-			'RECORD_USERS'			=> $l_online_record,
+			'TOTAL_USERS_ONLINE'	=> $data['.'][0]['TOTAL_USERS_ONLINE'],
+			'LOGGED_IN_USER_LIST'	=> $data['.'][0]['LOGGED_IN_USER_LIST'],
+			'RECORD_USERS'			=> $data['.'][0]['RECORD_USERS'],
 		));
+		unset($data);
 
 		return array(
 			'title'		=> $this->user->lang['WHO_IS_ONLINE'],
