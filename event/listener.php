3df4fce1ffcc8402ff0817ad77198ee5ae1152de
blitze <shizzah@hotmail.com>
2014-01-19 17:38:05 +0000

    process assets for acp

--- event/listener.php
+++ event/listener.php
@@ -24,6 +24,9 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 */
 class listener implements EventSubscriberInterface
 {
+	/* @var \phpbb\request\request_interface */
+	protected $request;
+
 	/* @var \primetime\primetime\core\primetime */
 	protected $primetime;
 	
@@ -33,11 +36,13 @@ class listener implements EventSubscriberInterface
 	/**
 	 * Constructor
 	 *
+	 * @param \phpbb\request\request_interface			$request		Request object
 	 * @param \primetime\primetime\core\primetime		$primetim		Primetime helper object
 	 * @param \primetime\primetime\core\blocks\display	$blocks			Blocks display object
 	*/
-	public function __construct(\primetime\primetime\core\primetime $primetime, \primetime\primetime\core\blocks\display $blocks)
+	public function __construct(\phpbb\request\request_interface $request, \primetime\primetime\core\primetime $primetime, \primetime\primetime\core\blocks\display $blocks)
 	{
+		$this->request = $request;
 		$this->primetime = $primetime;
 		$this->blocks = $blocks;
 	}
@@ -46,7 +51,9 @@ class listener implements EventSubscriberInterface
 	{
 		return array(
 			'core.user_setup'		=> 'load_language_on_setup',
+			'core.append_sid'		=> 'add_edit_mode',
 			'core.page_footer'		=> 'init',
+			'core.adm_page_footer'	=> 'set_assets',
 		);
 	}
 
@@ -60,10 +67,33 @@ class listener implements EventSubscriberInterface
 		$event['lang_set_ext'] = $lang_set_ext;
 	}
 
-	public function init($event)
+	public function add_edit_mode($event)
+	{
+		if ($this->request->is_set('edit_mode') && !preg_match('/ucp|mcp|adm/', $event['url']))
+		{
+			$params = $event['params'];
+			$amp = ($event['is_amp']) ? '&amp;' : '&';
+			if (!is_array($params))
+			{
+				$params .= (($params) ? $amp : '') . 'edit_mode=1';
+			}
+			else
+			{
+				$params[] = 'edit_mode=1';
+			}
+			$event['params'] = $params;
+		}
+	}
+
+	public function init()
 	{
 		$this->primetime->init();
 		$this->blocks->show();
+		$this->set_assets();
+	}
+
+	public function set_assets()
+	{
 		$this->primetime->set_assets();
 	}
 }
