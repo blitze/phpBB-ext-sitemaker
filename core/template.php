3593dd307c33d63dd36061740402a235b611e40f
blitze <shizzah@hotmail.com>
2014-02-12 00:54:05 +0000

    Primetime template is not only used by blocks

---
+++ core/template.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ * @package primetime
+ * @copyright (c) 2013 Daniel A. (blitze)
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace primetime\primetime\core;
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+*
+*/
+class template extends \phpbb\template\twig\twig
+{
+	public function __construct(\phpbb\path_helper $path_helper, $config, $user, \phpbb\template\context $context, \phpbb\extension\manager $extension_manager = null)
+	{
+		parent::__construct($path_helper, $config, $user, $context, $extension_manager);
+	}
+
+	public function clear()
+	{
+		$this->context->clear();
+	}
+}
