ec1e9ae2177078e2f1d065d1c4a7305f8757784c
blitze <dachebodt@gmail.com>
2014-03-08 22:27:06 +0000

    cache birthday block

--- blocks/birthday.php
+++ blocks/birthday.php
@@ -23,6 +23,12 @@ if (!defined('IN_PHPBB'))
 class birthday extends \primetime\primetime\core\blocks\driver\block
 {
 	/**
+	 * Cache
+	 * @var \phpbb\cache\service
+	 */
+	protected $cache;
+
+	/**
 	 * Database
 	 * @var \phpbb\db\driver\driver
 	 */
@@ -37,58 +43,70 @@ class birthday extends \primetime\primetime\core\blocks\driver\block
 	/**
 	 * Constructor
 	 *
+	 * @param \phpbb\cache\service			$cache		Cache object
 	 * @param \phpbb\db\driver\driver		$db     	Database connection
 	 * @param \phpbb\template\template		$user		User object
 	 */
-	public function __construct(\phpbb\db\driver\driver $db, \phpbb\user $user)
+	public function __construct(\phpbb\cache\driver\driver_interface $cache, \phpbb\db\driver\driver $db, \phpbb\user $user)
 	{
+		$this->cache = $cache;
 		$this->db = $db;
 		$this->user = $user;
 	}
 
 	public function display($bdata, $edit_mode = false)
 	{
-		$block = '';
-		$time = $this->user->create_datetime();
-		$now = phpbb_gmgetdate($time->getTimestamp() + $time->getOffset());
-	
-		// Display birthdays of 29th february on 28th february in non-leap-years
-		$leap_year_birthdays = '';
-		if ($now['mday'] == 28 && $now['mon'] == 2 && !$time->format('L'))
+		if (($content = $this->cache->get('_block_blocks' . $bdata['bid'])) === false)
 		{
-			$leap_year_birthdays = " OR u.user_birthday LIKE '" . $this->db->sql_escape(sprintf('%2d-%2d-', 29, 2)) . "%'";
+			$time = $this->user->create_datetime();
+			$now = phpbb_gmgetdate($time->getTimestamp() + $time->getOffset());
+
+			// Display birthdays of 29th february on 28th february in non-leap-years
+			$leap_year_birthdays = '';
+			if ($now['mday'] == 28 && $now['mon'] == 2 && !$time->format('L'))
+			{
+				$leap_year_birthdays = " OR u.user_birthday LIKE '" . $this->db->sql_escape(sprintf('%2d-%2d-', 29, 2)) . "%'";
+			}
+
+			$sql = 'SELECT u.user_id, u.username, u.user_colour, u.user_birthday 
+				FROM ' . USERS_TABLE . ' u
+				LEFT JOIN ' . BANLIST_TABLE . " b ON (u.user_id = b.ban_userid)
+				WHERE (b.ban_id IS NULL
+					OR b.ban_exclude = 1)
+					AND (u.user_birthday LIKE '" . $this->db->sql_escape(sprintf('%2d-%2d-', $now['mday'], $now['mon'])) . "%' $leap_year_birthdays)
+					AND u.user_type IN (" . USER_NORMAL . ', ' . USER_FOUNDER . ')';
+			$result = $this->db->sql_query($sql);
+
+			while ($row = $this->db->sql_fetchrow($result))
+			{
+				$birthday_username	= get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']);
+				$birthday_year		= (int) substr($row['user_birthday'], -4);
+				$birthday_age		= ($birthday_year) ? max(0, $now['year'] - $birthday_year) : '';
+
+				$this->ptemplate->assign_block_vars('birthday', array(
+					'USERNAME'		=> $birthday_username,
+					'USER_AGE'		=> $birthday_age,
+				));
+			}
+			$this->db->sql_freeresult($result);
+
+			$content = '';
+			if (!empty($row))
+			{
+				$content = $this->ptemplate->render_view('primetime/primetime', 'blocks/birthday.html', 'birthday_block');
+			}
+
+			// we only check birthdays every hour, may make this an admin choice
+			$this->cache->put('_block_blocks' . $bdata['bid'], $content, 3600);
 		}
-
-		$sql = 'SELECT u.user_id, u.username, u.user_colour, u.user_birthday 
-			FROM ' . USERS_TABLE . ' u
-			LEFT JOIN ' . BANLIST_TABLE . " b ON (u.user_id = b.ban_userid)
-			WHERE (b.ban_id IS NULL
-				OR b.ban_exclude = 1)
-				AND (u.user_birthday LIKE '" . $this->db->sql_escape(sprintf('%2d-%2d-', $now['mday'], $now['mon'])) . "%' $leap_year_birthdays)
-				AND u.user_type IN (" . USER_NORMAL . ', ' . USER_FOUNDER . ')';
-		$result = $this->db->sql_query($sql);
-
-		while ($row = $this->db->sql_fetchrow($result))
+		else if ($edit_mode !== false)
 		{
-			$birthday_username	= get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']);
-			$birthday_year		= (int) substr($row['user_birthday'], -4);
-			$birthday_age		= ($birthday_year) ? max(0, $now['year'] - $birthday_year) : '';
-
-			$this->ptemplate->assign_block_vars('birthday', array(
-				'USERNAME'		=> $birthday_username,
-				'USER_AGE'		=> $birthday_age,
-			));
-		}
-		$this->db->sql_freeresult($result);
-
-		if (!empty($row) || $edit_mode !== false)
-		{
-			$block = $this->ptemplate->render_view('primetime/primetime', 'blocks/birthday.html', 'birthday_block');
+			$content = $this->user->lang['BLOCK_NO_DATA'];
 		}
 
 		return array(
 			'title'		=> 'BIRTHDAYS',
-			'content'	=> $block,
+			'content'	=> $content,
 		);
 	}
 }
