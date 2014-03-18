62ae1a41fa66f71f0a8d557caff7e2f301817142
blitze <dachebodt@gmail.com>
2014-03-18 18:19:46 +0000

    Create parent forum category for primetime extnesions

--- migrations/v10x/v100.php
+++ migrations/v10x/v100.php
@@ -145,10 +145,14 @@ class v100 extends \phpbb\db\migration\migration
 	public function update_data()
 	{
 		return array(
+			array('custom', array(array($this, 'create_forum_cat'))),
+
 			array('config.add', array('primetime_enabled', true)),
 			array('config.add', array('primetime_version', '1.0.0')),
 			array('config.add', array('primetime_last_changed', 0)),
 			array('config.add', array('primetime_default_layout', '')),
+			array('config.add', array('primetime_parent_forum_id', 0)),
+			array('config.add', array('primetime_forum_perm_from', 0)),
 
 			array('permission.add', array('a_manage_blocks')),
 			array('permission.permission_set', array('ROLE_ADMIN_FULL', 'a_manage_blocks')),
@@ -185,7 +189,69 @@ class v100 extends \phpbb\db\migration\migration
 			array('config.remove', array('primetime_version')),
 			array('config.remove', array('primetime_last_changed')),
 			array('config.remove', array('primetime_default_layout')),
+			array('config.remove', array('primetime_parent_forum_id')),
+			array('config.remove', array('primetime_forum_perm_from')),
 			array('permission.remove', array('a_manage_blocks')),
 		);
+	}
+
+	public function create_forum_cat()
+	{
+		global $user;
+
+		$user->add_lang('acp/forums');
+
+		if (!class_exists('acp_forums'))
+		{
+		    include($this->phpbb_root_path . 'includes/acp/acp_forums.' . $this->php_ext);
+		}
+
+		$forum_data = array(
+			'parent_id'				=> 0,
+			'forum_type'			=> FORUM_CAT,
+			'type_action'			=> '',
+			'forum_status'			=> ITEM_UNLOCKED,
+			'forum_parents'			=> '',
+			'forum_name'			=> 'phpBB Primetime',
+			'forum_link'			=> '',
+			'forum_link_track'		=> false,
+			'forum_desc'			=> '',
+			'forum_desc_uid'		=> '',
+			'forum_desc_options'	=> 7,
+			'forum_desc_bitfield'	=> '',
+			'forum_rules'			=> '',
+			'forum_rules_uid'		=> '',
+			'forum_rules_options'	=> 7,
+			'forum_rules_bitfield'	=> '',
+			'forum_rules_link'		=> '',
+			'forum_image'			=> '',
+			'forum_style'			=> 0,
+			'display_subforum_list'	=> false,
+			'display_on_index'		=> false,
+			'forum_topics_per_page'	=> 0,
+			'enable_indexing'		=> true,
+			'enable_icons'			=> false,
+			'enable_prune'			=> false,
+			'enable_post_review'	=> true,
+			'enable_quick_reply'	=> false,
+			'prune_days'			=> 7,
+			'prune_viewed'			=> 7,
+			'prune_freq'			=> 1,
+			'prune_old_polls'		=> false,
+			'prune_announce'		=> false,
+			'prune_sticky'			=> false,
+			'show_active'			=> false,
+			'forum_password'		=> '',
+			'forum_password_confirm'=> '',
+			'forum_password_unset'	=> false,
+		);
+
+		$acp_forum = new \acp_forums();
+		$errors = $acp_forum->update_forum_data($forum_data);
+
+		if (!sizeof($errors))
+		{
+			$this->config->set('primetime_parent_forum_id', $forum_data['forum_id']);
+		}
 	}
 }
