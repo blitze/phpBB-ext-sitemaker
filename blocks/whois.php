74b508adf98cbe2f653f7ee4e1fb5a6fe00173c2
blitze <dachebodt@gmail.com>
2014-11-12 19:21:24 +0000

    Get who is online if not already available

--- blocks/whois.php
+++ blocks/whois.php
@@ -14,49 +14,64 @@ namespace primetime\primetime\blocks;
  */
 class whois extends \primetime\primetime\core\blocks\driver\block
 {
-	/**
-	 * phpBB configuration
-	 * @var \phpbb\config\config
-	 */
+	/** @var \phpbb\config\config */
 	protected $config;
 
-	/**
-	 * Template context
-	 * @var \phpbb\template\context
-	 */
+	/** @var \phpbb\template\context */
 	protected $context;
+
+	/** @var \phpbb\user */
+	protected $user;
 
 	/**
 	 * Constructor
 	 *
 	 * @param \phpbb\config\config		$config		phpBB configuration
 	 * @param \phpbb\template\context	$context    Template context
+	 * @param \phpbb\user				$user		User object
 	 */
-	public function __construct(\phpbb\config\config $config, \phpbb\template\context $context)
+	public function __construct(\phpbb\config\config $config, \phpbb\template\context $context, \phpbb\user $user)
 	{
 		$this->config = $config;
 		$this->context = $context;
+		$this->user = $user;
 	}
 
 	public function display($settings, $edit_mode = false)
 	{
 		$data = $this->context->get_data_ref();
 
-		$content = '';
 		if (!empty($data['.'][0]['TOTAL_USERS_ONLINE']))
 		{
-			$this->ptemplate->assign_vars(array(
-				'TOTAL_USERS_ONLINE'	=> $data['.'][0]['TOTAL_USERS_ONLINE'],
-				'LOGGED_IN_USER_LIST'	=> $data['.'][0]['LOGGED_IN_USER_LIST'],
-				'RECORD_USERS'			=> $data['.'][0]['RECORD_USERS'],
-			));
-			unset($data);
-			$content = $this->ptemplate->render_view('primetime/primetime', 'blocks/whois.html', 'whois_block');
+			$l_online_users	= $data['.'][0]['TOTAL_USERS_ONLINE'];
+			$online_userlist = $data['.'][0]['LOGGED_IN_USER_LIST'];
+			$l_online_record = $data['.'][0]['RECORD_USERS'];
 		}
+		else
+		{
+			$item_id = 0;
+			$item = 'forum';
+
+			$online_users = obtain_users_online($item_id, $item);
+			$user_online_strings = obtain_users_online_string($online_users, $item_id, $item);
+
+			$l_online_users = $user_online_strings['l_online_users'];
+			$online_userlist = $user_online_strings['online_userlist'];
+			$total_online_users = $online_users['total_online'];
+
+			$l_online_record = $this->user->lang('RECORD_ONLINE_USERS', (int) $this->config['record_online_users'], $this->user->format_date($this->config['record_online_date'], false, true));
+		}
+
+		$this->ptemplate->assign_vars(array(
+			'TOTAL_USERS_ONLINE'	=> $l_online_users,
+			'LOGGED_IN_USER_LIST'	=> $online_userlist,
+			'RECORD_USERS'			=> $l_online_record,
+		));
+		unset($data);
 
 		return array(
 			'title'		=> 'WHO_IS_ONLINE',
-			'content'	=> $content
+			'content'	=> $this->ptemplate->render_view('primetime/primetime', 'blocks/whois.html', 'whois_block')
 		);
 	}
 }
