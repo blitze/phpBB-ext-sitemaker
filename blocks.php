ea06f1e9ff86515889e57467704fe95d5d6175f9
blitze <dachebodt@gmail.com>
2014-11-12 16:56:44 +0000

    Coding Standards

--- blocks.php
+++ blocks.php
@@ -18,61 +18,70 @@ if (!defined('IN_PHPBB'))
 /**
  * Used to add multi-select dropdown in blocks config
  */
-function build_multi_select($option_ary, $selected_items, $key)
+if (!function_exists('build_multi_select'))
 {
-	global $user;
-
-	$selected_items = explode(',', $selected_items);
-
-	$html = '<select id="' . $key . '" name="config[' . $key . '][]" multiple="multiple">';
-	foreach ($option_ary as $value => $title)
+	function build_multi_select($option_ary, $selected_items, $key)
 	{
-		$title = (isset($user->lang[$title])) ? $user->lang[$title] : $title;
-		$selected = (in_array($value, $selected_items)) ? ' selected="selected"' : '';
-		$html .= '<option value="' . $value . '"' . $selected . '>' . $title . '</option>';
-	}
-	$html .= '</select>';
+		global $user;
 
-	return $html;
+		$selected_items = explode(',', $selected_items);
+
+		$html = '<select id="' . $key . '" name="config[' . $key . '][]" multiple="multiple">';
+		foreach ($option_ary as $value => $title)
+		{
+			$title = (isset($user->lang[$title])) ? $user->lang[$title] : $title;
+			$selected = (in_array($value, $selected_items)) ? ' selected="selected"' : '';
+			$html .= '<option value="' . $value . '"' . $selected . '>' . $title . '</option>';
+		}
+		$html .= '</select>';
+
+		return $html;
+	}
 }
 
 /**
  * Used to build multi-column checkboxes for blocks config
  */
-function build_checkbox($option_ary, $selected_items, $key)
+if (!function_exists('build_checkbox'))
 {
-	global $user;
-
-	$selected_items = explode(',', $selected_items);
-	$id_assigned = false;
-	$html = '';
-
-	$test = current($option_ary);
-	if (!is_array($test))
+	function build_checkbox($option_ary, $selected_items, $key)
 	{
-		$option_ary = array($option_ary);
-	}
+		global $user;
 
-	foreach ($option_ary as $col => $row)
-	{
-		$html .= '<div class="unit sizeAuto ' . $key . '-checkbox" id="' . $key . '-col-' . $col . '">';
-		foreach ($row as $value => $title)
+		$selected_items = explode(',', $selected_items);
+		$id_assigned = false;
+		$html = '';
+
+		$test = current($option_ary);
+		if (!is_array($test))
 		{
-			$selected = (in_array($value, $selected_items)) ? ' checked="checked"' : '';
-			$title = (isset($user->lang[$title])) ? $user->lang[$title] : $title;
-			$html .= '<label><input type="checkbox" name="config[' . $key . '][]"' . ((!$id_assigned) ? ' id="' . $key . '"' : '') . ' value="' . $value . '"' . $selected . (($key) ? ' accesskey="' . $key . '"' : '') . ' class="checkbox" /> ' . $title . '</label><br />';
-			$id_assigned = true;
+			$option_ary = array($option_ary);
 		}
-		$html .= '</div>';
-	}
 
-	return $html;
+		foreach ($option_ary as $col => $row)
+		{
+			$html .= '<div class="unit sizeAuto ' . $key . '-checkbox" id="' . $key . '-col-' . $col . '">';
+			foreach ($row as $value => $title)
+			{
+				$selected = (in_array($value, $selected_items)) ? ' checked="checked"' : '';
+				$title = (isset($user->lang[$title])) ? $user->lang[$title] : $title;
+				$html .= '<label><input type="checkbox" name="config[' . $key . '][]"' . ((!$id_assigned) ? ' id="' . $key . '"' : '') . ' value="' . $value . '"' . $selected . (($key) ? ' accesskey="' . $key . '"' : '') . ' class="checkbox" /> ' . $title . '</label><br />';
+				$id_assigned = true;
+			}
+			$html .= '</div>';
+		}
+
+		return $html;
+	}
 }
 
 /**
  * build hidden field for blocks config
  */
-function build_hidden($value, $key)
+if (!function_exists('build_hidden'))
 {
-	return '<input type="hidden" name="config[' . $key . ']" value="' . $value . '" />';
+	function build_hidden($value, $key)
+	{
+		return '<input type="hidden" name="config[' . $key . ']" value="' . $value . '" />';
+	}
 }
