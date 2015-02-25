cb2af251b52cb35fdf86c8417b1509c21cc66e41
blitze <dachebodt@gmail.com>
2015-02-25 20:26:31 +0000

    Send coverage

---
+++ travis/send_coverage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+DB=$1
+TRAVIS_PHP_VERSION=$2
+
+if [ "$TRAVIS_PHP_VERSION" == "5.5" -a "$DB" == "mysqli" ]
+then
+    phpBB/ext/$EXTNAME/vendor/bin/test-reporter
+    phpBB/ext/$EXTNAME/vendor/bin/coveralls -v
+fi
