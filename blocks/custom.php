ba0982b34de4a65207b9b9bef03ba64b4eca21dc
blitze <dachebodt@gmail.com>
2015-06-17 22:51:47 +0000

    Fix service name

--- blocks/custom.php
+++ blocks/custom.php
@@ -104,7 +104,7 @@ class custom extends \primetime\core\services\blocks\driver\block
 		if ($edit_mode !== false)
 		{
 			decode_message($cblock['block_content'], $cblock['bbcode_uid']);
-			$content = '<div id="block-editor-' . $cblock['block_id'] . '" class="editable-block" data-service="primetime.core.blocks.html" data-method="save" data-raw="' . $cblock['block_content'] . '">' . $content . '</div>';
+			$content = '<div id="block-editor-' . $cblock['block_id'] . '" class="editable-block" data-service="primetime.core.blocks.custom" data-method="save" data-raw="' . $cblock['block_content'] . '">' . $content . '</div>';
 		}
 
 		return array(
