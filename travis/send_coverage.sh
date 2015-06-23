3d11fcb023f008c08e4259391434005096871222
blitze <dachebodt@gmail.com>
2015-06-23 00:29:32 +0000

    Try to fix travis build

--- travis/send_coverage.sh
+++ travis/send_coverage.sh
@@ -6,7 +6,7 @@ set -x
 DB=$1
 TRAVIS_PHP_VERSION=$2
 
-if [ "$TRAVIS_PHP_VERSION" == "5.5" -a "$DB" == "mysqli" ]
+if [ "$TRAVIS_PHP_VERSION" == "5.4" -a "$DB" == "mysqli" ]
 then
     phpBB/ext/$EXTNAME/vendor/bin/test-reporter
     phpBB/ext/$EXTNAME/vendor/bin/coveralls -v
