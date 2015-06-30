ac3e4acc31858ba7c8959136a59199ff57138289
blitze <dachebodt@gmail.com>
2015-06-30 01:19:30 +0000

    Remove unnecessary double quote

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -9,7 +9,7 @@ EXTNAME=$3
 
 if [ "$TRAVIS_PHP_VERSION" == "5.5" -a "$DB" == "mysqli" ]
 then
-    phpBB/vendor/bin/phpunit --configuration phpBB/ext/$EXTNAME/travis/phpunit-$DB-travis.xml --bootstrap ./tests/bootstrap.php --coverage-clover build/logs/clover.xml;
+    phpBB/vendor/bin/phpunit --configuration phpBB/ext/$EXTNAME/travis/phpunit-$DB-travis.xml --bootstrap ./tests/bootstrap.php --coverage-clover build/logs/clover.xml
 else
-    phpBB/vendor/bin/phpunit --configuration phpBB/ext/$EXTNAME/travis/phpunit-$DB-travis.xml --bootstrap ./tests/bootstrap.php;
+    phpBB/vendor/bin/phpunit --configuration phpBB/ext/$EXTNAME/travis/phpunit-$DB-travis.xml --bootstrap ./tests/bootstrap.php
 fi
