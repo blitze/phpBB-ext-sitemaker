6f96a5b04e4fbb3eccaf09f0b6bb28b92a3d8d4b
blitze <dachebodt@gmail.com>
2015-07-15 00:15:36 +0000

    coverage directory

--- travis/send-coverage.sh
+++ travis/send-coverage.sh
@@ -5,9 +5,11 @@ set -x
 
 DB=$1
 TRAVIS_PHP_VERSION=$2
+EXTNAME=$3
 
 if [ "$TRAVIS_PHP_VERSION" == "5.5" -a "$DB" == "mysqli" ]
 then
+	cd phpBB/ext/$EXTNAME
     wget https://scrutinizer-ci.com/ocular.phar
-    php ocular.phar code-coverage:upload --format=php-clover ../../phpBB3/build/logs/clover.xml
+    php ocular.phar code-coverage:upload --format=php-clover ../../../../../phpBB3/build/logs/clover.xml
 fi
