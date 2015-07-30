8572de1a430fb550653d063030d60dab890b94f4
blitze <dachebodt@gmail.com>
2015-07-30 07:36:37 +0000

    Remove unused Use statement

--- services/blocks/manager.php
+++ services/blocks/manager.php
@@ -10,7 +10,6 @@
 namespace blitze\sitemaker\services\blocks;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use blitze\sitemaker\services\blocks\cfg_fields;
 
 class manager extends route
 {
