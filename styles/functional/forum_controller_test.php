adb47d63fe8b1f89b98da130084f9110de561a62
blitze <dachebodt@gmail.com>
2016-03-23 18:05:09 +0000

    Fix issue #23

---
+++ styles/functional/forum_controller_test.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * @package sitemaker
+ * @copyright (c) 2015 Daniel A. (blitze)
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace blitze\sitemaker\tests\functional;
+
+/**
+ * @group functional
+ */
+class forum_controller_test extends \phpbb_functional_test_case
+{
+	/**
+	 * Define the extensions to be tested
+	 *
+	 * @return array vendor/name of extension(s) to test
+	 * @access static
+	 */
+	static protected function setup_extensions()
+	{
+		return array('blitze/sitemaker');
+	}
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->add_lang_ext('blitze/sitemaker', array(
+			'common',
+		));
+	}
+
+    public function test_forum_controller()
+    {
+        $crawler = self::request('GET', 'app.php/forum');
+        $this->assertGreaterThan(0, $crawler->filter('.topiclist')->count());
+    }
+}
