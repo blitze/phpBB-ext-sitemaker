5049ea500d975ad68f232f0d777db01db31d20fe
blitze <dachebodt@gmail.com>
2016-08-27 04:14:00 +0000

    Remove duplicated schema update - defined in m12_add_hidden_forum_column

--- migrations/v20x/m1_initial_schema.php
+++ migrations/v20x/m1_initial_schema.php
@@ -102,11 +102,6 @@ class m1_initial_schema extends \phpbb\db\migration\migration
 					),
 				),
 			),
-			'add_columns'	=> array(
-				$this->table_prefix . 'forums'	=> array(
-					'hidden_forum'		=> array('BOOL', 0),
-				),
-			),
 		);
 	}
 
