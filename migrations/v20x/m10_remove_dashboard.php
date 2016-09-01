c332dab602cd9b996d13367d0a8700d4f62cfa1b
blitze <dachebodt@gmail.com>
2016-09-01 14:48:08 +0000

    Only remove dashboard if it exists

--- migrations/v20x/m10_remove_dashboard.php
+++ migrations/v20x/m10_remove_dashboard.php
@@ -29,23 +29,36 @@ class m10_remove_dashboard extends \phpbb\db\migration\migration
 	 */
 	public function update_data()
 	{
+		$dashboard_exists = $this->module_exists('ACP_SITEMAKER_EXTENSIONS');
+
 		return array(
-			// Remove all ACP modules
-			array('module.remove', array('acp', 'ACP_SITEMAKER_EXTENSIONS', array(
-				'module_langname'	=> 'MENU',
-				'module_mode'		=> 'menu',
-			))),
-
-			array('module.remove', array('acp', 'ACP_CAT_SITEMAKER', array(
-				'module_langname'	=> 'SITEMAKER_DASHBOARD',
-				'module_mode'		=> 'dashboard',
-			))),
-
-			array('module.remove', array('acp', 'SITEMAKER', 'ACP_SITEMAKER_EXTENSIONS')),
-
-			array('module.remove', array('acp', 'SITEMAKER', 'ACP_CAT_SITEMAKER')),
-
+			// Remove all ACP modules, if one exists
+			array('if', array(
+				($dashboard_exists),
+				array('module.remove', array('acp', 'ACP_SITEMAKER_EXTENSIONS', array(
+					'module_langname'	=> 'MENU',
+					'module_mode'		=> 'menu',
+				))),
+			)),
+			array('if', array(
+				($dashboard_exists),
+				array('module.remove', array('acp', 'ACP_CAT_SITEMAKER', array(
+					'module_langname'	=> 'SITEMAKER_DASHBOARD',
+					'module_mode'		=> 'dashboard',
+				))),
+			)),
+			array('if', array(
+				($dashboard_exists),
+				array('module.remove', array('acp', 'SITEMAKER', 'ACP_SITEMAKER_EXTENSIONS')),
+			)),
+			array('if', array(
+				($dashboard_exists),
+				array('module.remove', array('acp', 'SITEMAKER', 'ACP_CAT_SITEMAKER')),
+			)),
+			array('if', array(
+				($dashboard_exists),
 			array('module.remove', array('acp', 0, 'SITEMAKER')),
+			)),
 
 			// Add Menu module to Extensions tab
 			array('module.add', array('acp', 'ACP_CAT_DOT_MODS', 'ACP_SITEMAKER')),
@@ -56,5 +69,21 @@ class m10_remove_dashboard extends \phpbb\db\migration\migration
 				),
 			)),
 		);
+	}
+
+	/**
+	 * @param string $langname
+	 * @return bool
+	 */
+	function module_exists($langname)
+	{
+		$sql = 'SELECT module_langname 
+			FROM ' . $this->table_prefix . "modules
+			WHERE module_langname='" . $this->db->sql_escape($langname) . "'";
+		$result = $this->db->sql_query($sql);
+		$exists = $this->db->sql_fetchfield('module_langname');
+		$this->db->sql_freeresult($result);
+
+		return ($exists) ? true : false;
 	}
 }
