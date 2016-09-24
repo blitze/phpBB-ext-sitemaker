b56b52ade7c67a4b72e31df1e0bfbe3a3a25d924
blitze <dachebodt@gmail.com>
2016-09-24 16:33:05 +0000

    Fix epv?

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -11,12 +11,12 @@
 set -e
 set -x
 
-DB=$1
-TRAVIS_PHP_VERSION=$2
+EPV=$1
+NOTESTS=$2
 
-if [ "$DB" == "mysqli" -a "$TRAVIS_PHP_VERSION" == "5.5" ]
+if [ "$EPV" == "1" -a "$NOTESTS" == "1" ]
 then
 	cd phpBB
-	composer require phpbb/epv:dev-master#5238b08ae3ad434c7bac6baba0cb1afe1e13b012 --dev --no-interaction
+	composer require phpbb/epv:dev-master# --dev --no-interaction
 	cd ../
 fi
