35910295b940ce0c0abaf6040ff827c82c7cd9c0
blitze <dachebodt@gmail.com>
2017-07-19 19:11:51 +0000

    Attempt to fix trvis build

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -16,6 +16,7 @@ NOTESTS=$1
 if [ "$NOTESTS" == "1" ]
 then
 	cd phpBB
-	composer require phpbb/epv:dev-master --dev --no-interaction
+	composer remove sami/sami --update-with-dependencies --dev --no-interaction
+	composer require phpbb/epv:dev-master --dev --no-interaction --ignore-platform-reqs
 	cd ../
 fi
