82d2cc61185cf18757e0f8441699bf0abff971ad
blitze <dachebodt@gmail.com>
2017-07-19 19:13:02 +0000

    Strictly require phpBB 3.2.1

---
+++ ext.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * @package sitemaker
+ * @copyright (c) 2013 Daniel A. (blitze)
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace blitze\sitemaker;
+
+class ext extends \phpbb\extension\base
+{
+	public function is_enableable()
+	{
+		$config = $this->container->get('config');
+		return phpbb_version_compare($config['version'], '3.2.1', '>=');
+	}
+}
