e691b1164ede28e545c0e1423f357907a7454b09
Daniel A <dachebodt@gmail.com>
2017-09-25 02:47:26 +0000

    Cannot use extension service before extension is enabled

--- migrations/v30x/m302_filemanager.php
+++ migrations/v30x/m302_filemanager.php
@@ -31,7 +31,7 @@ class m302_filemanager extends \phpbb\db\migration\container_aware_migration
 	{
 		return array(
 			array('config.add', array('sm_navbar_menu', 0)),
-			array('config.add', array('sm_filemanager', false)),
+			array('config.add', array('sm_filemanager', true)),
 
 			array('permission.add', array('u_sm_filemanager', true, 'u_attach')),
 
@@ -45,13 +45,14 @@ class m302_filemanager extends \phpbb\db\migration\container_aware_migration
 	public function create_upload_dir()
 	{
 		$fs = $this->container->get('filesystem');
-		$filemanager = $this->container->get('blitze.sitemaker.filemanager');
 
 		// create upload folders if they don't exist
 		$fs->mkdir($this->phpbb_root_path . 'images/sitemaker_uploads/source', 0766);
 		$fs->mkdir($this->phpbb_root_path . 'images/sitemaker_uploads/thumbs', 0744);
 
 		// set filemanager config
-		$filemanager->init();
+		$config_template = $this->phpbb_root_path . 'ext/blitze/sitemaker/services/filemanager/default.config';
+		$config_file = $this->phpbb_root_path . 'ext/blitze/sitemaker/vendor/ResponsiveFilemanager/filemanager/config/config.' . $this->php_ext;
+		$fs->copy($config_template, $config_file, true);
 	}
 }
