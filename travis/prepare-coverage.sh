3147926009cb351bc7fdba9182e5fc763f655301
blitze <dachebodt@gmail.com>
2019-01-24 16:59:53 +0000

    Update travis stuff

--- travis/prepare-coverage.sh
+++ travis/prepare-coverage.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
-# Collapsible Categories extension for the phpBB Forum Software package.
+# This file is part of the phpBB Forum Software package.
 #
-# @copyright (c) 2015 phpBB Limited <https://www.phpbb.com>
+# @copyright (c) phpBB Limited <https://www.phpbb.com>
 # @license GNU General Public License, version 2 (GPL-2.0)
+#
+# For full copyright and license information, please see
+# the docs/CREDITS.txt file.
 #
 set -e
 set -x
