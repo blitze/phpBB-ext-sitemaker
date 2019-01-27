7a1ea04300a899fbc01bf357cdbb2efbb08b96b5
blitze <dachebodt@gmail.com>
2019-01-27 23:43:32 +0000

    Only show loader and messages for sitemaker requests

--- develop/components/Ajax/setup.js
+++ develop/components/Ajax/setup.js
@@ -2,25 +2,35 @@
 const { lang } = window;
 
 export default function Ajaxer($loader, showMessage, append = '') {
+	let sitemakerRequest = false;
 	$.ajaxSetup({
 		// add style and session ids to ajax requests
 		beforeSend: (xhr, settings) => {
-			$loader.addClass('fa-spinner fa-green fa-spin fa-lg fa-pulse');
+			if (settings.url.indexOf('/sitemaker') > -1) {
+				sitemakerRequest = true;
+				$loader.addClass('fa-spinner fa-green fa-spin fa-lg fa-pulse');
 
-			if (append) {
-				const delim = settings.url.indexOf('?') < 0 ? '?' : '&';
-				settings.url += `${delim}${append}`;
+				if (append) {
+					const delim = settings.url.indexOf('?') < 0 ? '?' : '&';
+					settings.url += `${delim}${append}`;
+				}
 			}
 		},
 		complete: ({ responseJSON }) => {
-			$loader
-				.delay(2000)
-				.removeClass('fa-spinner fa-green fa-spin fa-lg fa-pulse');
+			if (sitemakerRequest) {
+				$loader
+					.delay(2000)
+					.removeClass('fa-spinner fa-green fa-spin fa-lg fa-pulse');
 
-			// Display any returned message
-			if (responseJSON.message || responseJSON.MESSAGE_TEXT) {
-				showMessage(responseJSON.message || responseJSON.MESSAGE_TEXT);
+				// Display any returned message
+				if (responseJSON.message || responseJSON.MESSAGE_TEXT) {
+					showMessage(
+						responseJSON.message || responseJSON.MESSAGE_TEXT,
+					);
+				}
 			}
+
+			sitemakerRequest = false;
 		},
 		error: event => {
 			showMessage(
--- tests/functional/startpage_test.php
+++ tests/functional/startpage_test.php
@@ -111,7 +111,7 @@ class startpage_test extends \phpbb_functional_test_case
 	private function make_async_request(array $data)
 	{
         $client = new \GuzzleHttp\Client(['base_url' => self::$root_url]);
-		return $client->post('app.php/blocks/set_startpage?style=1', [
+		return $client->post('app.php/sitemaker/blocks/set_startpage?style=1', [
 			'cookies'	=> true,
 			'future'	=> true,
 		    'json'		=> $data,
