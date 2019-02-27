9c3f4549251318b8f372bc46650545b58024d466
blitze <shizzah@hotmail.com>
2019-02-26 18:16:09 -0600

    New translations common.php (Czech)

---
+++ language/cs/common.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ *
+ * @package phpBB Sitemaker [English]
+ * @copyright (c) 2013 Daniel A. (blitze)
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$strings = array(
+	'ALL_TIME'						=> 'All Time',
+
+	'BLOCK_TITLE'					=> 'Block Title',
+
+	'CHANGE_ME'						=> 'Change Me',
+
+	'DAILY_MEMBER'					=> 'Member of the day',
+
+	'FEATURED_MEMBER'				=> 'Featured Member',
+	'FEATURED_MEMBERLIST'			=> 'Featured members list',
+	'FORUM_ANNOUNCEMENTS'			=> 'Forum Announcements',
+	'FORUM_GLOBAL_ANNOUNCEMENTS'	=> 'Global Forum Announcements',
+	'FORUM_RECENT_POSTS'			=> 'Recent Forum Posts',
+	'FORUM_RECENT_TOPICS'			=> 'Recent Forum Topics',
+	'FORUM_STICKY_POSTS'			=> 'Recent Sticky Posts',
+
+	'HELP'							=> 'Help',
+	'HOURLY_MEMBER'					=> 'Member of the hour',
+
+	'JOIN_DATE'						=> 'Join Date',
+
+	'LAST_POST_BY_AUTHOR'			=> 'Last post by',
+	'LAST_VISITED'					=> 'Last Visited',
+	'LINKS'							=> 'Links',
+
+	'MCP_SITEMAKER_CONTENT'			=> 'Contents',
+	'MEMBERS_DATE'					=> 'Date',
+	'MENU'							=> 'Menu',
+	'MONTHLY_MEMBER'				=> 'Member of the month',
+	'MOST_TENURED'					=> 'Most Tenured',
+	'MY_BOOKMARKS'					=> 'My Bookmarks',
+
+	'NO_BOOKMARKED_TOPICS'			=> 'You have not bookmarked any topics',
+	'NO_NEW_POSTS'					=> 'There are no new posts to display',
+	'NO_NEW_TOPICS'					=> 'There are no new topics to display',
+
+	'POLL'							=> 'Poll',
+	'POSTS_MEMBER'					=> 'Top Poster',
+	'PROCESSING'					=> 'processing...',
+
+	'QTYPE_POSTS'					=> 'Congratulations to:',
+	'QTYPE_RECENT'					=> 'Please welcome our newest member:',
+
+	'RECENT_BOTS'					=> 'Recent Search Engines',
+	'RECENT_MEMBER'					=> 'Recent Member',
+	'RECENT_MEMBERS'				=> 'Recent Members',
+
+	'SESSION_HIDE_ME'				=> 'Hide Me',
+	'SM_NAVIGATION'					=> 'Navigation',
+	'SM_TOGGLE_DROPDOWN'			=> 'Toggle dropdown',
+	'STYLE_SWITCHER'				=> 'Style Switcher',
+
+	'THIS_MONTH'					=> 'This Month',
+	'THIS_WEEK'						=> 'This Week',
+	'THIS_YEAR'						=> 'This Year',
+	'TODAY'							=> 'Today',
+	'TOPICS_LAST_READ'				=> 'Last Read Topics',
+	'TOPIC_LAST_READ'				=> 'Last read %s',
+	'TOP_POSTERS'					=> 'Top Posters',
+
+	'UCP_SITEMAKER_CONTENT'			=> 'My Stuff',
+
+	'VIEW_DETAILS'					=> 'View Details',
+	'VIEW_USER_PROFILE'				=> 'All about %s',
+
+	'WEEKLY_MEMBER'					=> 'Member of the week',
+	'WELCOME'						=> 'Welcome',
+	'WHATS_NEW'						=> 'What’s New?',
+	'WORDGRAPH'						=> 'Wordgraph',
+);
+
+$lang = array_merge($lang, $strings);
