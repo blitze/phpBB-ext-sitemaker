b78b1298e5a6957b58f9f3665c0143b9a4f357d4
blitze <shizzah@hotmail.com>
2019-03-10 00:58:28 +0000

    Add CodeMirror controls for undo/redo/clear/fullscreen

--- develop/components/CodeMirror/index.js
+++ develop/components/CodeMirror/index.js
@@ -1,9 +1,13 @@
 /* global $ */
 import CodeMirror from 'codemirror';
+import 'codemirror/mode/htmlmixed/htmlmixed';
 import 'codemirror/addon/display/autorefresh';
-import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/display/fullscreen';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/monokai.css';
+import 'codemirror/addon/display/fullscreen.css';
+
+import './style.scss';
 
 let codeMirror = {};
 
@@ -22,23 +26,49 @@ export function insertText(data) {
 	}
 }
 
-export function initCodeMirror(textarea) {
-	codeMirror = CodeMirror.fromTextArea($(textarea).get(0), {
+export function initCodeMirror(textarea, overwrites = {}) {
+	const $textarea = $(textarea);
+	const $buttons = $('.CodeMirror-button');
+	const settings = {
 		theme: 'monokai',
+		mode: 'htmlmixed',
 		lineWrapping: false,
 		autoRefresh: true,
-		coverGutterNextToScrollbar: false,
 		indentWithTabs: true,
 		lineNumbers: true,
 		indentUnit: 4,
-	});
+		extraKeys: {
+			F11: cm => {
+				cm.setOption('fullScreen', !cm.getOption('fullScreen'));
+				cm.focus();
+			},
+			Esc: cm => {
+				if (cm.getOption('fullScreen')) {
+					cm.setOption('fullScreen', false);
+				}
+			},
+		},
+
+		// overwrites
+		...overwrites,
+	};
+
+	codeMirror = CodeMirror.fromTextArea($textarea.get(0), settings);
 
 	let cmTabs = 0;
+	let $undoRedo = {};
+
 	codeMirror.on('keyup', cm => {
 		const doc = cm.getDoc();
 		const line = doc.getLine(cm.getCursor().line);
 		const matches = line.match(/\s/gim);
 		cmTabs = matches ? matches.length : 0;
+	});
+
+	codeMirror.on('keyHandled', (cm, name, e) => {
+		if (name === 'Esc') {
+			e.stopPropagation();
+		}
 	});
 
 	codeMirror.on('change', (cm, change) => {
@@ -53,7 +83,49 @@ export function initCodeMirror(textarea) {
 				cm.indentLine(line, spaces);
 			}
 		});
+
+		if ($undoRedo.length) {
+			const historySize = codeMirror.getDoc().historySize();
+			$undoRedo.each((i, el) => {
+				const action = $(el).data('action');
+				$(el).prop('disabled', historySize[action] < 1);
+			});
+		}
 	});
+
+	codeMirror.on('blur', () => {
+		$textarea.val(codeMirror.getValue());
+	});
+
+	if ($buttons.length) {
+		const doAction = {
+			undo: () => codeMirror.undo(),
+			redo: () => codeMirror.redo(),
+			clear: () => codeMirror.setValue(''),
+			fullscreen: () => {
+				codeMirror.setOption(
+					'fullScreen',
+					!codeMirror.getOption('fullScreen'),
+				);
+				codeMirror.focus();
+			},
+		};
+
+		$buttons.click(e => {
+			e.preventDefault();
+			const action = $(e.currentTarget).data('action');
+			if (doAction[action]) {
+				doAction[action]();
+			}
+		});
+
+		$undoRedo = $buttons
+			.filter((i, el) => {
+				const action = $(el).data('action');
+				return action === 'undo' || action === 'redo';
+			})
+			.prop('disabled', true);
+	}
 
 	return codeMirror;
 }
