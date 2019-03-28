c1d6c4ea2b4d9d2bb4af68f08b4eadb28564b102
blitze <shizzah@hotmail.com>
2019-03-28 19:57:39 +0000

    Make this more testable

--- develop/entries/settings/index.js
+++ develop/entries/settings/index.js
@@ -1,36 +1,10 @@
 /* global $ */
+import LayoutPreviewer from '../../components/LayoutPreviewer';
 import '../../components/Icons/picker';
 import './settings.scss';
 
-let $preview;
-let currPreview = '';
-const defaultPreview = './images/spacer.gif';
-
-function showPreview(layout) {
-	if (layout !== currPreview) {
-		$preview.error(() => {
-			$preview.attr('src', defaultPreview);
-		});
-		$preview.attr('src', `${layout}preview.png`);
-		currPreview = layout;
-	}
-}
-
 $(document).ready(() => {
-	$preview = $('.layout-preview').find('img');
-
-	$('.style-layouts > dl')
-		.hover(e => {
-			const layout = $(e.currentTarget).data('layout');
-			showPreview(layout);
-		})
-		.find('.layout-option')
-		.change(e => {
-			const layout = e.currentTarget.value;
-			$(e.currentTarget)
-				.closest('dl')
-				.data('layout', layout);
-		});
+	LayoutPreviewer();
 
 	// Init icon picker
 	$('#acp_settings').iconPicker({
