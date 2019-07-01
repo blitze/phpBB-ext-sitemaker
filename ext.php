a662cc788ab8340c09885e8b7ef857907134e1c6
blitze <shizzah@hotmail.com>
2019-07-01 05:06:05 +0000

    Get required phpBB version from composer

--- ext.php
+++ ext.php
@@ -11,9 +11,85 @@ namespace blitze\sitemaker;
 
 class ext extends \phpbb\extension\base
 {
+	/**
+	 * Check whether or not the extension can be enabled.
+	 *
+	 * @return bool
+	 */
 	public function is_enableable()
 	{
-		$config = $this->container->get('config');
-		return phpbb_version_compare($config['version'], '3.2.1', '>=');
+		$user = $this->container->get('user');
+		$user->add_lang_ext('blitze/sitemaker', 'ext');
+
+		$lang = $user->lang;
+		$metadata = $this->get_metadata('blitze/sitemaker');
+
+		$req_phpbb_version = $metadata['extra']['soft-require']['phpbb/phpbb'];
+		$phpbb_version = $this->container->get('config')['version'];
+		$is_enableable = $this->version_is_ok($phpbb_version, $req_phpbb_version);
+
+		if (!$is_enableable)
+		{
+			$lang['EXTENSION_NOT_ENABLEABLE'] .= '<br>' . $user->lang('PHPBB_VERSION_UNMET', $req_phpbb_version);
+		}
+
+		$user->lang = $lang;
+		return $is_enableable;
+	}
+
+	/**
+	 * @param string $current_version
+	 * @param string $required_version
+	 * @return bool
+	 */
+	protected function version_is_ok($current_version, &$required_version)
+	{
+		$required_version = html_entity_decode($required_version);
+		list($min_req_version, $max_req_version) = explode(',', $required_version);
+
+		$contraint = $this->get_version_constraint($min_req_version);
+		if (!phpbb_version_compare($current_version, $contraint['version'], $contraint['operator']))
+		{
+			return false;
+		}
+
+		$contraint = $this->get_version_constraint($max_req_version);
+		if ($constraint['version'] && !phpbb_version_compare($current_version, $contraint['version'], $contraint['operator']))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param string $version
+	 * @return array
+	 */
+	protected function get_version_constraint($version)
+	{
+		if (preg_match('/^(\D+)(.+)/i', trim($version), $matches))
+		{
+			list(, $operator, $version) = $matches;
+		}
+
+		return [
+			'version' => str_replace('@', '', $version),
+			'operator' => $operator ?: '>=',
+		];
+	}
+
+	/**
+	 * Get composer metadata information
+	 *
+	 * @param string $name
+	 * @param string $element
+	 * @return array
+	 */
+	protected function get_metadata($name, $element = 'all')
+	{
+		$ext_manager = $this->container->get('ext.manager');
+		$metadata_manager = $ext_manager->create_extension_metadata_manager($name);
+		return $metadata_manager->get_metadata($element);
 	}
 }
--- tests/ext_test.php
+++ tests/ext_test.php
@@ -2,7 +2,7 @@
 /**
  *
  * @package sitemaker
- * @copyright (c) 2017 Daniel A. (blitze)
+ * @copyright (c) 2019 Daniel A. (blitze)
  * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
  *
  */
@@ -11,21 +11,54 @@ namespace blitze\sitemaker\tests;
 
 class ext_test extends \phpbb_test_case
 {
-	protected $ext;
-
 	/**
-	 * Configure the test environment.
-	 *
-	 * @return void
+	 * @param string $required_phpbb_version
+	 * @return \blitze\sitemaker\ext
 	 */
-	public function setUp()
+	public function get_ext($required_phpbb_version)
 	{
-		parent::setUp();
+		$config = new \phpbb\config\config(['version' => '3.2.7']);
 
-		$this->config = new \phpbb\config\config(array());
+		$translator = $this->getMockBuilder('\phpbb\language\language')
+			->disableOriginalConstructor()
+			->getMock();
+		$translator->expects($this->any())
+			->method('lang')
+			->willReturnCallback(function () {
+				return implode('-', func_get_args());
+			});
+
+		$this->user = new \phpbb\user($translator, '\phpbb\datetime');
+		$this->user->lang = ['EXTENSION_NOT_ENABLEABLE' => ''];
 
 		$container = new \phpbb_mock_container_builder();
-		$container->set('config', $this->config);
+
+		$ext_manager = $this->getMockBuilder('\phpbb\extension\manager')
+			->disableOriginalConstructor()
+			->getMock();
+		$ext_manager->expects($this->any())
+			->method('create_extension_metadata_manager')
+			->willReturnCallback(function($name) use ($required_phpbb_version) {
+				$metadata_manager = new \phpbb_mock_metadata_manager($name, '');
+				$metadata = [
+					'name'		=> $name,
+					'type'		=> 'phpbb-extension',
+					'license'	=> 'GPL2',
+					'authors'	=> [['name'	=> 'foo bar']],
+					'version'	=> '3.0.0',
+					'extra'		=> [
+						'soft-require'	=> [
+							'phpbb/phpbb'	=> $required_phpbb_version,
+						],
+					],
+				];
+				$metadata_manager->set_metadata($metadata);
+				return $metadata_manager;
+			});
+
+		$container->set('user', $this->user);
+		$container->set('config', $config);
+		$container->set('ext.manager', $ext_manager);
 
 		$finder = $this->getMockBuilder('\phpbb\finder')
 			->disableOriginalConstructor()
@@ -35,7 +68,7 @@ class ext_test extends \phpbb_test_case
 			->disableOriginalConstructor()
 			->getMock();
 
-		$this->ext = new \blitze\sitemaker\ext($container, $finder, $migrator, 'some_ext', 'some_path');
+		return new \blitze\sitemaker\ext($container, $finder, $migrator, 'some_ext', 'some_path');
 	}
 
 	/**
@@ -44,19 +77,37 @@ class ext_test extends \phpbb_test_case
 	public function ext_test_data()
 	{
 		return array(
-			array('3.2.0', false),
-			array('3.2.1', true),
+			array(
+				'3.2.8',
+				false,
+				'<br>PHPBB_VERSION_UNMET-3.2.8',
+			),
+			array(
+				'>=3.2.8,<3.3.0@dev',
+				false,
+				'<br>PHPBB_VERSION_UNMET->=3.2.8,<3.3.0@dev',
+			),
+			array(
+				'>=3.2.7,<3.3.0@dev',
+				true,
+				'',
+			),
 		);
 	}
 
 	/**
 	 * @dataProvider ext_test_data
+	 * @param string $required_phpbb_version
+	 * @param bool $expected_result
+	 * @param string $expect_message
 	 * @return void
 	 */
-	public function test_is_enableable($phpbb_version, $expected)
+	public function test_is_enableable($required_phpbb_version, $expected_result, $expected_message)
 	{
-		$this->config->set('version', $phpbb_version);
+		$ext = $this->get_ext($required_phpbb_version);
+		$result = $ext->is_enableable();
 
-		$this->assertEquals($expected, $this->ext->is_enableable());
+		$this->assertEquals($expected_result, $result);
+		$this->assertEquals($expected_message, $this->user->lang['EXTENSION_NOT_ENABLEABLE']);
 	}
 }
