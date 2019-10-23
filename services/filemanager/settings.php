4c754f92c4f1aa7f989d36850c5542bb14c2ac24
blitze <shizzah@hotmail.com>
2019-10-23 00:40:47 +0000

    Ensure filemanager config is automatically updated to latest version

--- services/filemanager/settings.php
+++ services/filemanager/settings.php
@@ -27,7 +27,7 @@ class settings
 	protected $config_template;
 
 	/** @var string */
-	protected $config_version = '3.1.1';
+	protected $config_version = '3.2.0';
 
 	/** @var array */
 	protected $filemanager_prop_types = array(
