e263c007057aacc638c7659e1c4b623bcbaf7f7b
blitze <shizzah@hotmail.com>
2020-02-28 16:05:16 -0600

    New translations permissions_sitemaker.php (Dutch) [ci skip]

--- language/nl/permissions_sitemaker.php
+++ language/nl/permissions_sitemaker.php
@@ -38,11 +38,11 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	// Admin Permissions
-	'ACL_A_SM_SETTINGS'			=> 'Can manage Sitemaker settings',
-	'ACL_A_SM_MANAGE_BLOCKS'	=> 'Can manage Sitemaker blocks',
-	'ACL_A_SM_MANAGE_MENUS'		=> 'Can manage Sitemaker menus',
-	'ACL_A_SM_FILEMANAGER'		=> 'Can see/manage other users’ folders in File Manager',
+	'ACL_A_SM_SETTINGS'			=> 'Kan sitemaker instellingen beheren',
+	'ACL_A_SM_MANAGE_BLOCKS'	=> 'Kan sitemaker blokken beheren',
+	'ACL_A_SM_MANAGE_MENUS'		=> 'Kan Sitemaker menu\'s beheren',
+	'ACL_A_SM_FILEMANAGER'		=> 'Kan mappen van andere gebruikers in bestandsbeheer zoeken/beheren',
 
 	// User Permissions
-	'ACL_U_SM_FILEMANAGER'		=> 'Can use File Manager',
+	'ACL_U_SM_FILEMANAGER'		=> 'Kan Bestandsbeheer gebruiken',
 ));
