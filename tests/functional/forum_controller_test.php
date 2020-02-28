a8cdcd029bd3f5c3677ec72f8a4f8af383a599c7
blitze <shizzah@hotmail.com>
2020-02-28 06:34:06 +0000

    Whoopsie!

--- tests/functional/forum_controller_test.php
+++ tests/functional/forum_controller_test.php
@@ -30,7 +30,6 @@ class forum_controller_test extends \phpbb_functional_test_case
 		$this->add_lang_ext('blitze/sitemaker', 'common');
 
 		$crawler = self::request('GET', 'app.php/forum');
-		print $crawler->filter('#nav-main')->text();
         $this->assertGreaterThan(0, $crawler->filter('.topiclist')->count());
     }
 }
