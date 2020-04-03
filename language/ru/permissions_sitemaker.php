c8aa0e27df4e152aebf8ee365de1384ca8a08daf
blitze <shizzah@hotmail.com>
2020-04-02 20:59:16 -0500

    New translations permissions_sitemaker.php (Russian) [ci skip]

--- language/ru/permissions_sitemaker.php
+++ language/ru/permissions_sitemaker.php
@@ -38,11 +38,11 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	// Admin Permissions
-	'ACL_A_SM_SETTINGS'			=> 'Can manage Sitemaker settings',
-	'ACL_A_SM_MANAGE_BLOCKS'	=> 'Can manage Sitemaker blocks',
-	'ACL_A_SM_MANAGE_MENUS'		=> 'Can manage Sitemaker menus',
-	'ACL_A_SM_FILEMANAGER'		=> 'Can see/manage other users’ folders in File Manager',
+	'ACL_A_SM_SETTINGS'			=> 'Может управлять настройками Sitemaker',
+	'ACL_A_SM_MANAGE_BLOCKS'	=> 'Управление блоками Sitemaker',
+	'ACL_A_SM_MANAGE_MENUS'		=> 'Позволяет управлять меню Sitemaker',
+	'ACL_A_SM_FILEMANAGER'		=> 'Может просматривать/управлять папками других пользователей в файловом менеджере',
 
 	// User Permissions
-	'ACL_U_SM_FILEMANAGER'		=> 'Can use File Manager',
+	'ACL_U_SM_FILEMANAGER'		=> 'Может использовать файловый менеджер',
 ));
