2aab31884ff96b4085f581d1472f4988c95f331c
blitze <shizzah@hotmail.com>
2020-08-04 14:00:36 -0500

    Add brand icons to picker

--- develop/components/Icons/categories.js
+++ develop/components/Icons/categories.js
@@ -3,15 +3,17 @@ const request = require('request');
 const axios = require('axios');
 const YAML = require('yamljs');
 
-const categoriesSrc = 'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/5.13.0/metadata/categories.yml';
-const iconsSrc = 'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/5.13.0/metadata/icons.yml'
+const categoriesSrc =
+	'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/5.13.0/metadata/categories.yml';
+const iconsSrc =
+	'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/5.13.0/metadata/icons.yml';
 const outputYML = 'services/icons/categories.yml';
 
 async function getJson() {
 	try {
 		const [res1, res2] = await Promise.all([
 			axios.get(categoriesSrc),
-			axios.get(iconsSrc)
+			axios.get(iconsSrc),
 		]);
 		return [res1.data, res2.data];
 	} catch (error) {
@@ -21,8 +23,15 @@ async function getJson() {
 
 async function getCategories() {
 	const [categoriesYML, iconsYML] = await getJson();
-	const categoriesJson = YAML.parse(categoriesYML);
 	const iconsJson = YAML.parse(iconsYML);
+
+	let categoriesJson = YAML.parse(categoriesYML);
+
+	categoriesJson['brands'] = {
+		icons: Object.keys(iconsJson).filter(x =>
+			iconsJson[x].styles.includes('brands'),
+		),
+	};
 
 	const newCategories = Object.keys(categoriesJson).reduce((cats, name) => {
 		let { icons } = categoriesJson[name];
@@ -30,13 +39,13 @@ async function getCategories() {
 			return {
 				name: icon,
 				terms: [...iconsJson[icon].search.terms, icon].join('|'),
-				prefixes: iconsJson[icon].styles.map(style => `fa${style[0]}`)
-			}
+				prefixes: iconsJson[icon].styles.map(style => `fa${style[0]}`),
+			};
 		});
 
 		return {
 			...cats,
-			[name]: { icons }
+			[name]: { icons },
 		};
 	}, {});
 
