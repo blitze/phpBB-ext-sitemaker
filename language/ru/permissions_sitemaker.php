63b3344949342d60dfa31bc86f5c8463b966d16e
blitze <shizzah@hotmail.com>
2020-08-05 01:47:57 -0500

    New translations permissions_sitemaker.php (Russian) [ci skip]

--- language/ru/permissions_sitemaker.php
+++ language/ru/permissions_sitemaker.php
@@ -1,22 +1,21 @@
 <?php
-/**
-*
-* @package phpBB Sitemaker [English]
- * @copyright (c) 2013 Daniel A. (blitze)
-* @license http://opensource.org/licenses/gpl-license.php GNU Public License
-*
-*/
 
 /**
-* DO NOT CHANGE
-*/
-if (!defined('IN_PHPBB'))
-{
+ *
+ * @package phpBB Sitemaker [English]
+ * @copyright (c) 2013 Daniel A. (blitze)
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB')) {
 	exit;
 }
 
-if (empty($lang) || !is_array($lang))
-{
+if (empty($lang) || !is_array($lang)) {
 	$lang = array();
 }
 
@@ -38,11 +37,11 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	// Admin Permissions
-	'ACL_A_SM_SETTINGS'			=> 'Может управлять настройками Sitemaker',
-	'ACL_A_SM_MANAGE_BLOCKS'	=> 'Управление блоками Sitemaker',
-	'ACL_A_SM_MANAGE_MENUS'		=> 'Позволяет управлять меню Sitemaker',
-	'ACL_A_SM_FILEMANAGER'		=> 'Может просматривать/управлять папками других пользователей в файловом менеджере',
+	'ACL_A_SM_SETTINGS'			=> '[ SiteMaker ] Can manage settings',
+	'ACL_A_SM_MANAGE_BLOCKS'	=> '[ SiteMaker ] Can manage blocks',
+	'ACL_A_SM_MANAGE_MENUS'		=> '[ SiteMaker ] Can manage menus',
+	'ACL_A_SM_FILEMANAGER'		=> '[ SiteMaker ] Can see/manage other users’ folders in File Manager',
 
 	// User Permissions
-	'ACL_U_SM_FILEMANAGER'		=> 'Может использовать файловый менеджер',
+	'ACL_U_SM_FILEMANAGER'		=> '[ SiteMaker ] Can use File Manager',
 ));
