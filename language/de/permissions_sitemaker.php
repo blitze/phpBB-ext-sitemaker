541b157953d4ef773d03643d836fd9661fe21241
blitze <shizzah@hotmail.com>
2020-08-05 12:20:28 -0500

    New translations permissions_sitemaker.php (German) [ci skip]

--- language/de/permissions_sitemaker.php
+++ language/de/permissions_sitemaker.php
@@ -11,11 +11,13 @@
 /**
  * DO NOT CHANGE
  */
-if (!defined('IN_PHPBB')) {
+if (!defined('IN_PHPBB'))
+{
 	exit;
 }
 
-if (empty($lang) || !is_array($lang)) {
+if (empty($lang) || !is_array($lang))
+{
 	$lang = array();
 }
 
