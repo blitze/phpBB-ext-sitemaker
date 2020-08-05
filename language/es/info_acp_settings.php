a02fe01f37bf33f08fbd29add1900716735471d6
blitze <shizzah@hotmail.com>
2020-08-05 01:48:53 -0500

    New translations info_acp_settings.php (Spanish) [ci skip]

--- language/es/info_acp_settings.php
+++ language/es/info_acp_settings.php
@@ -48,7 +48,7 @@ $lang = array_merge($lang, [
 	'FILEMANAGER_SETTINGS'						=> 'Configuración del Administrador de Archivos',
 	'FILEMANAGER_STATUS'						=> 'Estado',
 	'FILEMANAGER_NO_EXIST'						=> 'You will need to install the File Manager before you can enable it. Installation instructions are found <a href="%s" target="_blank"  rel="noopener noreferrer"><strong>here</strong></a>',
-	'FILEMENAGER_NOT_WRITABLE'					=> 'File manager config folder (root/ResponsiveFilemanager/filemanager/config/) is not writable. Please change the permissions to writable by all (777 or -rwxrwxrwx within your FTP Client)',
+	'FILEMANAGER_NOT_WRITABLE'					=> 'File manager config folder (root/ResponsiveFilemanager/filemanager/config/) is not writable. Please change the permissions to writable by all (777 or -rwxrwxrwx within your FTP Client)',
 	'FILEMANAGER_IMAGE_AUTO_RESIZE'				=> '¿Redimensionar automáticamente las imágenes subidas?',
 	'FILEMANAGER_IMAGE_AUTO_RESIZE_DIMENSIONS'	=> 'Redimensionar a dimensiones especificadas',
 	'FILEMANAGER_IMAGE_AUTO_RESIZING_MODE'		=> 'Modo redimensionado automático',
