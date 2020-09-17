4a7221e24451b34f76303f92bb3cc9e4de6866df
blitze <shizzah@hotmail.com>
2020-09-17 12:03:54 -0500

    Do not commit binary files

---
+++ travis/prepare-asset-bundling.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This file is part of the phpBB Forum Software package.
+#
+# @copyright (c) phpBB Limited <https://www.phpbb.com>
+# @license GNU General Public License, version 2 (GPL-2.0)
+#
+# For full copyright and license information, please see
+# the docs/CREDITS.txt file.
+#
+set -e
+set -x
+
+EXTNAME=$1
+
+# Move eslint config back in place
+cp ../tmp/.eslintrc phpBB/ext/$EXTNAME
+
+# Move yarn lock file back in place
+cp ../tmp/yarn.lock phpBB/ext/$EXTNAME
+
+# Install npm dependencies
+yarn --cwd phpBB/ext/$EXTNAME install
