fcc29e3cf0d64e79b5aea9d96b27e2d7411e56ea
blitze <shizzah@hotmail.com>
2021-06-21 16:05:16 -0500

    New translations exceptions.php (Italian) [ci skip]

--- language/it/exceptions.php
+++ language/it/exceptions.php
@@ -37,13 +37,13 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'EXCEPTION_FIELD_MISSING'		=> 'Required field missing',
-	'EXCEPTION_INVALID_ACTION'		=> 'The action does not exist',
-	'EXCEPTION_INVALID_ARGUMENT'	=> 'Invalid argument specified for `%1$s`. Reason: %2$s',
-	'EXCEPTION_INVALID_DATA_TYPE'	=> 'The provided value is of an unexpected data type',
-	'EXCEPTION_INVALID_ENTITY'		=> 'The provided entity is of an unexpected entity class',
-	'EXCEPTION_INVALID_PROPERTY'	=> 'The requested property does not exist',
-	'EXCEPTION_OUT_OF_BOUNDS'		=> 'The requested `%1$s` does not exist',
-	'EXCEPTION_SERVICE_NOT_FOUND'	=> 'The requested service was not found',
-	'EXCEPTION_UNEXPECTED_VALUE'	=> 'The requested action `%1$s` could not be performed. Reason: %2$s',
+	'EXCEPTION_FIELD_MISSING'		=> 'Campo obbligatorio mancante',
+	'EXCEPTION_INVALID_ACTION'		=> 'L\'azione non esiste',
+	'EXCEPTION_INVALID_ARGUMENT'	=> 'Argomento non valido specificato per `%1$s`. Motivo: %2$s',
+	'EXCEPTION_INVALID_DATA_TYPE'	=> 'Il valore fornito è di un tipo di dati imprevisto',
+	'EXCEPTION_INVALID_ENTITY'		=> 'L’entità fornita è di una classe di entità imprevista',
+	'EXCEPTION_INVALID_PROPERTY'	=> 'La proprietà richiesta non esiste',
+	'EXCEPTION_OUT_OF_BOUNDS'		=> 'La richiesta `%1$s` non esiste',
+	'EXCEPTION_SERVICE_NOT_FOUND'	=> 'Il servizio richiesto non è stato trovato',
+	'EXCEPTION_UNEXPECTED_VALUE'	=> 'L\'azione richiesta `%1$s` non può essere eseguita. Motivo: %2$s',
 ));
