fa02bcfbf15ac4ec996a7fc1471d44ff848cd22b
blitze <shizzah@hotmail.com>
2021-06-21 16:06:37 -0500

    New translations ext.php (Romanian) [ci skip]

--- language/ro/ext.php
+++ language/ro/ext.php
@@ -37,6 +37,6 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'IMAGE_DIRECTORY_NOT_WRITABLE'	=> 'The phpBB Image directory is not writable. Please change the permissions to writable by all (777 or -rwxrwxrwx within your FTP Client)',
-	'PHPBB_VERSION_UNMET'			=> 'This extension requires at least phpBB %s',
+	'IMAGE_DIRECTORY_NOT_WRITABLE'	=> 'Directorul phpBB Imagine nu permite scrierea. Vă rugăm să schimbați permisiunile pentru a putea fi scris de toți (777 sau -rwxrwxrwx în cadrul FTP Client)',
+	'PHPBB_VERSION_UNMET'			=> 'Această extensie necesită cel puţin phpBB %s',
 ));
