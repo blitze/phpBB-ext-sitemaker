48d6f568620bc2232a89e179a8be545de276c05c
blitze <shizzah@hotmail.com>
2021-06-21 16:00:14 -0500

    New translations info_acp_menus.php (Dutch) [ci skip]

--- language/nl/info_acp_menus.php
+++ language/nl/info_acp_menus.php
@@ -38,55 +38,55 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'ACP_MENU'					=> 'Menu',
-	'ACP_MENU_MANAGE'			=> 'Menubeheer',
-	'ACP_MENU_MANAGE_EXPLAIN'	=> 'Hier kunt u menu\'s maken en beheren voor uw site',
-	'ADD_BULK_MENU'				=> 'Bulk toevoegen menu-items',
-	'ADD_BULK_MENU_EXPLAIN'		=> 'Meerdere menu-items tegelijk toevoegen.<br /> - Plaats elk item op een aparte regel<br /> - Gebruik de <strong>Tab</strong> toets voor streepjes om ouder-kindrelaties te vertegenwoordigen<br /> - Voer item en URL zoals ze: Home|index.php',
+	'ACP_MENU_MANAGE'			=> 'Menu beheer',
+	'ACP_MENU_MANAGE_EXPLAIN'	=> 'Hier kunt u menu\'s voor uw site maken en beheren',
+	'ADD_BULK_MENU'				=> 'Bulk menu-items toevoegen',
+	'ADD_BULK_MENU_EXPLAIN'		=> 'Meerdere menu-items tegelijk toevoegen.<br /> - Plaats elk item op een aparte regel<br /> - Gebruik de <strong>Tab</strong> toets om items in te laten springen om ouder-onderliggende relaties te vertegenwoordigen<br /> - Voer item en URL zoals bijvoorbeeld Homedad index.php',
 	'ADD_MENU'					=> 'Menu toevoegen',
 	'ADD_MENU_ITEM'				=> 'Menu-item toevoegen',
 	'ADD_ITEM'					=> 'Nieuw item toevoegen',
 	'AJAX_PROCESSING'			=> 'Werken',
 
-	'CHANGE_ME'					=> 'Wijzigen',
+	'CHANGE_ME'					=> 'Verander mij',
 
 	'DELETE_ITEM'				=> 'Item verwijderen',
-	'DELETE_KIDS'				=> 'Verwijder branch',
-	'DELETE_MENU'				=> 'Menu verwijderen',
+	'DELETE_KIDS'				=> 'Aftakking verwijderen',
+	'DELETE_MENU'				=> 'Verwijder Menu',
 	'DELETE_MENU_CONFIRM'		=> 'Weet u zeker dat u dit menu wilt verwijderen?<br />Dit zal het menu en alle items verwijderen',
 	'DELETE_MENU_ITEM'			=> 'Item verwijderen',
-	'DELETE_MENU_ITEM_CONFIRM'	=> 'Weet u zeker dat u dit menu-item wilt verwijderen?',
-	'DELETE_SELECTED'			=> 'Verwijder geselecteerde',
+	'DELETE_MENU_ITEM_CONFIRM'	=> 'Weet je zeker dat je dit menu-item wilt verwijderen?',
+	'DELETE_SELECTED'			=> 'Geselecteerde verwijderen',
 
 	'EDIT_ITEM'					=> 'Item bewerken',
 
-	'ITEM_ACTIVE'				=> 'Actief',
+	'ITEM_ACTIVE'				=> 'actief',
 	'ITEM_INACTIVE'				=> 'Inactief',
-	'ITEM_PARENT'				=> 'Bovenliggend',
-	'ITEM_TITLE'				=> 'Item titel',
-	'ITEM_TITLE_EXPLAIN'		=> 'Instellen als \'-\' voor provider',
-	'ITEM_TARGET'				=> 'Artikel doel',
-	'ITEM_URL'					=> 'Item URL',
-	'ITEM_URL_EXPLAIN'			=> '- Laat leeg voor titels<br />- Externe sites moeten beginnen met http(s)://, ftp://, /, etc',
+	'ITEM_PARENT'				=> 'Bovenliggende',
+	'ITEM_TITLE'				=> 'Artikel titel',
+	'ITEM_TITLE_EXPLAIN'		=> 'Instellen als \'\' voor de deler',
+	'ITEM_TARGET'				=> 'Item Target',
+	'ITEM_URL'					=> 'Artikel URL',
+	'ITEM_URL_EXPLAIN'			=> '- Laat leeg voor kopteksten<br />- Externe websites moeten beginnen met http(s)://, ftp://, //, etc',
 
-	'MENU_ITEMS'				=> 'Menu-items',
+	'MENU_ITEMS'				=> 'Menu items',
 
 	'NO_MENU_ITEMS'				=> 'Er zijn geen menu-items aangemaakt',
-	'NO_PARENT'					=> 'Geen ouder',
+	'NO_PARENT'					=> 'Geen bovenliggende pagina',
 
-	'PROCESSING_ERROR'			=> 'Verwerking fout',
+	'PROCESSING_ERROR'			=> 'Fout bij verwerken',
 
-	'REBUILD_TREE'				=> 'Herbouw Boom',
-	'REQUIRED'					=> 'Vereist',
+	'REBUILD_TREE'				=> 'Boom opnieuw opbouwen',
+	'REQUIRED'					=> 'vereist',
 	'REQUIRED_FIELDS'			=> '* Verplichte velden',
 
 	'SAVE_CHANGES'				=> 'Wijzigingen opslaan',
 	'SAVE'						=> 'Opslaan',
-	'SELECT_ALL'				=> 'Selecteer alles',
+	'SELECT_ALL'				=> 'Alles selecteren',
 
 	'TARGET_BLANK'				=> 'Lege pagina',
-	'TARGET_PARENT'				=> 'Bovenliggend',
+	'TARGET_PARENT'				=> 'Bovenliggende',
 
-	'UNSAVED_CHANGES'			=> 'U heeft niet-opgeslagen wijzigingen',
+	'UNSAVED_CHANGES'			=> 'Je hebt niet-opgeslagen wijzigingen',
 
-	'VISIT_PAGE'				=> 'Bezoek pagina',
+	'VISIT_PAGE'				=> 'Pagina bezoeken',
 ));
