89fa1ac3b3ce3805cc0ccbad89ad63a96dca921b
blitze <shizzah@hotmail.com>
2021-06-21 16:00:48 -0500

    New translations exceptions.php (Portuguese) [ci skip]

--- language/pt/exceptions.php
+++ language/pt/exceptions.php
@@ -37,13 +37,13 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'EXCEPTION_FIELD_MISSING'		=> 'Falta o campo obrigatório',
+	'EXCEPTION_FIELD_MISSING'		=> 'Campo obrigatório faltando',
 	'EXCEPTION_INVALID_ACTION'		=> 'A ação não existe',
 	'EXCEPTION_INVALID_ARGUMENT'	=> 'Argumento inválido especificado para `%1$s`. Motivo: %2$s',
 	'EXCEPTION_INVALID_DATA_TYPE'	=> 'O valor fornecido é de um tipo de dado inesperado',
-	'EXCEPTION_INVALID_ENTITY'		=> 'A entidade fornecida é de uma classe inesperada de entidade',
+	'EXCEPTION_INVALID_ENTITY'		=> 'A entidade fornecida é de uma classe de entidade inesperada',
 	'EXCEPTION_INVALID_PROPERTY'	=> 'A propriedade solicitada não existe',
-	'EXCEPTION_OUT_OF_BOUNDS'		=> 'O requisitado `%1$s` não existe',
+	'EXCEPTION_OUT_OF_BOUNDS'		=> 'O pedido `%1$s` não existe',
 	'EXCEPTION_SERVICE_NOT_FOUND'	=> 'O serviço solicitado não foi encontrado',
 	'EXCEPTION_UNEXPECTED_VALUE'	=> 'A ação solicitada `%1$s` não pôde ser executada. Motivo: %2$s',
 ));
