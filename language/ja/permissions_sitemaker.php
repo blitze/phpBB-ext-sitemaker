7bbc21e7d799af5d1a14fe8aa619c42695a70b90
blitze <shizzah@hotmail.com>
2021-06-21 14:33:32 -0500

    New translations permissions_sitemaker.php (Japanese) [ci skip]

--- language/ja/permissions_sitemaker.php
+++ language/ja/permissions_sitemaker.php
@@ -39,7 +39,7 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	// Admin Permissions
-	'ACL_A_SM_SETTINGS'			=> '[ SiteMaker ] Can manage settings',
-	'ACL_A_SM_MANAGE_BLOCKS'	=> '[ SiteMaker ] Can manage blocks',
-	'ACL_A_SM_MANAGE_MENUS'		=> '[ SiteMaker ] Can manage menus',
+	'ACL_A_SM_SETTINGS'			=> '[ SiteMaker ] 設定を管理可能',
+	'ACL_A_SM_MANAGE_BLOCKS'	=> '[ SiteMaker ] ブロックを管理可能',
+	'ACL_A_SM_MANAGE_MENUS'		=> '[ SiteMaker ] メニューを管理可能',
 ));
