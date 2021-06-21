4e2f75f0bafdd91be142cfee60c069fc1ff064d1
blitze <shizzah@hotmail.com>
2021-06-21 15:56:15 -0500

    New translations ext.php (Danish) [ci skip]

--- language/da/ext.php
+++ language/da/ext.php
@@ -37,6 +37,6 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'IMAGE_DIRECTORY_NOT_WRITABLE'	=> 'The phpBB Image directory is not writable. Please change the permissions to writable by all (777 or - rwxrwxrwx within your FTP Client) (Automatic Copy)',
-	'PHPBB_VERSION_UNMET'			=> 'Denne udvidelse kræver mindst phpBB %s',
+	'IMAGE_DIRECTORY_NOT_WRITABLE'	=> 'The phpBB Image directory is not writable. Please change the permissions to writable by all (777 or -rwxrwxrwx within your FTP Client)',
+	'PHPBB_VERSION_UNMET'			=> 'This extension requires at least phpBB %s',
 ));
