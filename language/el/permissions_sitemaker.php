e5518e2a841bfeba37fb668e1945e7dcf4e30644
blitze <shizzah@hotmail.com>
2021-06-21 15:56:20 -0500

    New translations permissions_sitemaker.php (Greek) [ci skip]

--- language/el/permissions_sitemaker.php
+++ language/el/permissions_sitemaker.php
@@ -39,7 +39,7 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	// Admin Permissions
-	'ACL_A_SM_SETTINGS'			=> '[ SiteMaker ] Μπορεί να διαχειριστεί τις ρυθμίσεις',
-	'ACL_A_SM_MANAGE_BLOCKS'	=> '[ SiteMaker ] Μπορεί να διαχειριστεί μπλοκ',
-	'ACL_A_SM_MANAGE_MENUS'		=> '[ SiteMaker ] Μπορεί να διαχειριστεί μενού',
+	'ACL_A_SM_SETTINGS'			=> '[ SiteMaker ] Can manage settings',
+	'ACL_A_SM_MANAGE_BLOCKS'	=> '[ SiteMaker ] Can manage blocks',
+	'ACL_A_SM_MANAGE_MENUS'		=> '[ SiteMaker ] Can manage menus',
 ));
