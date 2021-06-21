f158b0ade4acaed430a03ec968b2906ac85226b9
blitze <shizzah@hotmail.com>
2021-06-21 15:56:37 -0500

    New translations permissions_sitemaker.php (Arabic) [ci skip]

--- language/ar/permissions_sitemaker.php
+++ language/ar/permissions_sitemaker.php
@@ -39,7 +39,7 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	// Admin Permissions
-	'ACL_A_SM_SETTINGS'			=> '[SiteMaker ] يمكن إدارة الإعدادات',
-	'ACL_A_SM_MANAGE_BLOCKS'	=> '[SiteMaker ] يمكن إدارة الكتل',
-	'ACL_A_SM_MANAGE_MENUS'		=> '[SiteMaker ] يمكن إدارة القوائم',
+	'ACL_A_SM_SETTINGS'			=> '[ SiteMaker ] Can manage settings',
+	'ACL_A_SM_MANAGE_BLOCKS'	=> '[ SiteMaker ] Can manage blocks',
+	'ACL_A_SM_MANAGE_MENUS'		=> '[ SiteMaker ] Can manage menus',
 ));
