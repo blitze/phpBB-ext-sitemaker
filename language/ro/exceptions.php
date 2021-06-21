ffbeffde11ff8ad4a3d288a53ce3944072670147
blitze <shizzah@hotmail.com>
2021-06-21 15:55:05 -0500

    New translations exceptions.php (Romanian) [ci skip]

--- language/ro/exceptions.php
+++ language/ro/exceptions.php
@@ -37,13 +37,13 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'EXCEPTION_FIELD_MISSING'		=> 'Câmp obligatoriu lipsă',
-	'EXCEPTION_INVALID_ACTION'		=> 'Acțiunea nu există',
-	'EXCEPTION_INVALID_ARGUMENT'	=> 'Argument nevalid specificat pentru `%1$s`. Motiv: %2$s',
-	'EXCEPTION_INVALID_DATA_TYPE'	=> 'Valoarea furnizată este de un tip de date neașteptat',
-	'EXCEPTION_INVALID_ENTITY'		=> 'Entitatea furnizată se află într-o clasă de entități neașteptată',
-	'EXCEPTION_INVALID_PROPERTY'	=> 'Proprietatea solicitată nu există',
+	'EXCEPTION_FIELD_MISSING'		=> 'Required field missing',
+	'EXCEPTION_INVALID_ACTION'		=> 'The action does not exist',
+	'EXCEPTION_INVALID_ARGUMENT'	=> 'Invalid argument specified for `%1$s`. Reason: %2$s',
+	'EXCEPTION_INVALID_DATA_TYPE'	=> 'The provided value is of an unexpected data type',
+	'EXCEPTION_INVALID_ENTITY'		=> 'The provided entity is of an unexpected entity class',
+	'EXCEPTION_INVALID_PROPERTY'	=> 'The requested property does not exist',
 	'EXCEPTION_OUT_OF_BOUNDS'		=> 'The requested `%1$s` does not exist',
-	'EXCEPTION_SERVICE_NOT_FOUND'	=> 'Serviciul solicitat nu a fost găsit',
-	'EXCEPTION_UNEXPECTED_VALUE'	=> 'Acţiunea solicitată `%1$snu a putut fi efectuată. Motivul: %2$s',
+	'EXCEPTION_SERVICE_NOT_FOUND'	=> 'The requested service was not found',
+	'EXCEPTION_UNEXPECTED_VALUE'	=> 'The requested action `%1$s` could not be performed. Reason: %2$s',
 ));
