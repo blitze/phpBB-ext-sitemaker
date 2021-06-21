1766ed61ae7bbd36082bf972f9090ec41ff7fc44
blitze <shizzah@hotmail.com>
2021-06-21 16:00:31 -0500

    New translations common.php (Portuguese) [ci skip]

--- language/pt/common.php
+++ language/pt/common.php
@@ -22,76 +22,76 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ALL_TIME'						=> 'Todo o tempo',
+	'ALL_TIME'						=> 'Todos os tempos',
 
-	'BLOCK_TITLE'					=> 'Título do bloco',
+	'BLOCK_TITLE'					=> 'Título do Bloco',
 
-	'CHANGE_ME'						=> 'Mude-me',
+	'CHANGE_ME'						=> 'Alterar para mim',
 
 	'DAILY_MEMBER'					=> 'Membro do dia',
 
 	'FEATURED_MEMBER'				=> 'Membro em destaque',
-	'FEATURED_MEMBERLIST'			=> 'Lista de membros em destaque',
-	'FEEDS'							=> 'Feeds',
-	'FORUM_ANNOUNCEMENTS'			=> 'Anúncios do fórum',
-	'FORUM_GLOBAL_ANNOUNCEMENTS'	=> 'Anúncios globais do fórum',
-	'FORUM_RECENT_POSTS'			=> 'Postagens Recentes no Fórum',
-	'FORUM_RECENT_TOPICS'			=> 'Tópicos recentes do Fórum',
-	'FORUM_STICKY_POSTS'			=> 'Posts recentes Sticky',
+	'FEATURED_MEMBERLIST'			=> 'Lista de participantes destacados',
+	'FEEDS'							=> 'Conteúdos',
+	'FORUM_ANNOUNCEMENTS'			=> 'Avisos do fórum',
+	'FORUM_GLOBAL_ANNOUNCEMENTS'	=> 'Anúncios de Fórum Global',
+	'FORUM_RECENT_POSTS'			=> 'Posts Recentes do Fórum',
+	'FORUM_RECENT_TOPICS'			=> 'Tópicos Recentes do Fórum',
+	'FORUM_STICKY_POSTS'			=> 'Publicações Recentes de Fixação',
 
-	'HELP'							=> 'Ajuda',
+	'HELP'							=> 'Socorro',
 	'HOURLY_MEMBER'					=> 'Membro da hora',
 
-	'GOOGLE_MAP'					=> 'Mapa do Google',
+	'GOOGLE_MAP'					=> 'Google Mapa',
 
-	'JOIN_DATE'						=> 'Data de adesão',
+	'JOIN_DATE'						=> 'Data de entrada',
 
-	'LAST_POST_BY_AUTHOR'			=> 'Último post por',
+	'LAST_POST_BY_AUTHOR'			=> 'Última publicação por',
 	'LAST_VISITED'					=> 'Última visita',
 	'LINKS'							=> 'Links',
 
 	'MCP_SITEMAKER_CONTENT'			=> 'Conteúdo',
-	'MEMBERS_DATE'					=> 'Data',
+	'MEMBERS_DATE'					=> 'Encontro',
 	'MENU'							=> 'Menu',
 	'MONTHLY_MEMBER'				=> 'Membro do mês',
-	'MOST_TENURED'					=> 'Mais Tenured',
+	'MOST_TENURED'					=> 'Mais tensas',
 	'MY_BOOKMARKS'					=> 'Meus Favoritos',
 
 	'NO_BOOKMARKED_TOPICS'			=> 'Você não marcou nenhum tópico como favorito',
-	'NO_NEW_TOPICS'					=> 'Não há tópicos novos para exibir',
+	'NO_NEW_TOPICS'					=> 'Não há novos tópicos para exibir',
 
 	'POLL'							=> 'Enquete',
 	'POPULAR_TOPICS'				=> 'Tópicos Populares',
-	'POSTS_MEMBER'					=> 'Maior Cartaz',
-	'PROCESSING'					=> 'processando...',
+	'POSTS_MEMBER'					=> 'Cartaz superior',
+	'PROCESSING'					=> 'Processandochar@@0',
 
-	'QTYPE_POSTS'					=> 'Parabéns a:',
-	'QTYPE_RECENT'					=> 'Por favor, dê as boas-vindas ao nosso membro mais recente:',
+	'QTYPE_POSTS'					=> 'Parabéns para:',
+	'QTYPE_RECENT'					=> 'Por favor, seja bem-vindo ao nosso mais recente usuário:',
 
-	'RECENT_BOTS'					=> 'Motores de pesquisa recentes',
+	'RECENT_BOTS'					=> 'Motores de busca recentes',
 	'RECENT_MEMBER'					=> 'Membro recente',
 	'RECENT_MEMBERS'				=> 'Membros recentes',
 
-	'SESSION_HIDE_ME'				=> 'Ocultar-me',
-	'SM_NAVIGATION'					=> 'Navegação',
-	'SM_TOGGLE_DROPDOWN'			=> 'Alternar lista suspensa',
+	'SESSION_HIDE_ME'				=> 'Me Esconder',
+	'SM_NAVIGATION'					=> 'Navigation',
+	'SM_TOGGLE_DROPDOWN'			=> 'Ativar/Desativar dropdown',
 	'STYLE_SWITCHER'				=> 'Alternador de estilo',
 
-	'THIS_MONTH'					=> 'Este mês',
+	'THIS_MONTH'					=> 'Este Mês',
 	'THIS_WEEK'						=> 'Esta semana',
 	'THIS_YEAR'						=> 'Este Ano',
-	'TODAY'							=> 'Hoje',
+	'TODAY'							=> 'hoje',
 	'TOPICS_LAST_READ'				=> 'Últimos tópicos lidos',
 	'TOPIC_LAST_READ'				=> 'Última leitura %s',
-	'TOP_POSTERS'					=> 'Top Posts',
+	'TOP_POSTERS'					=> 'Melhores cartazes',
 
-	'UCP_SITEMAKER_CONTENT'			=> 'Minhas coisas',
+	'UCP_SITEMAKER_CONTENT'			=> 'Minhas Coisas',
 
-	'VIEW_DETAILS'					=> 'Ver detalhes',
+	'VIEW_DETAILS'					=> 'Visualizar Detalhes',
 	'VIEW_USER_PROFILE'				=> 'Tudo sobre %s',
 
 	'WEEKLY_MEMBER'					=> 'Membro da semana',
 	'WELCOME'						=> 'Boas-vindas',
-	'WHATS_NEW'						=> 'O que é novo?',
+	'WHATS_NEW'						=> 'Quais as novidades?',
 	'WORDGRAPH'						=> 'Wordgraph',
 ));
