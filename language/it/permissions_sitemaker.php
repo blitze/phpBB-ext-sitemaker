7fd97ba6eb0c276e4a5f65bc11a5f155bd814ca6
blitze <shizzah@hotmail.com>
2021-06-21 14:33:12 -0500

    New translations permissions_sitemaker.php (Italian) [ci skip]

--- language/it/permissions_sitemaker.php
+++ language/it/permissions_sitemaker.php
@@ -39,7 +39,7 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	// Admin Permissions
-	'ACL_A_SM_SETTINGS'			=> '[ SiteMaker ] Can manage settings',
-	'ACL_A_SM_MANAGE_BLOCKS'	=> '[ SiteMaker ] Can manage blocks',
-	'ACL_A_SM_MANAGE_MENUS'		=> '[ SiteMaker ] Can manage menus',
+	'ACL_A_SM_SETTINGS'			=> '[ SiteMaker ] Può gestire le impostazioni',
+	'ACL_A_SM_MANAGE_BLOCKS'	=> '[ SiteMaker ] Può gestire i blocchi',
+	'ACL_A_SM_MANAGE_MENUS'		=> '[ SiteMaker ] Può gestire i menu',
 ));
