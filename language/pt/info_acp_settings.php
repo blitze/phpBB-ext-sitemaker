093f97fdfe0d56f277c061d320dc884ad05b7ad6
blitze <shizzah@hotmail.com>
2021-06-21 16:00:15 -0500

    New translations info_acp_settings.php (Portuguese) [ci skip]

--- language/pt/info_acp_settings.php
+++ language/pt/info_acp_settings.php
@@ -36,39 +36,39 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, [
 	'ACP_SITEMAKER'		=> 'SiteMaker',
-	'ACP_SM_SETTINGS'	=> 'Configurações',
+	'ACP_SM_SETTINGS'	=> 'Confirgurações',
 
-	'BLOCKS_CLEANUP'			=> 'Limpeza de blocos',
-	'BLOCKS_CLEANUP_EXPLAIN'	=> 'Os seguintes itens não foram encontrados mais ou inacessíveis, e você pode, portanto, excluir todos os blocos associados a eles. Por favor, tenha em mente que alguns destes podem ser falsos positivos',
-	'BLOCKS_CLEANUP_BLOCKS'		=> 'Blocos inválidos (ex. de extensões desinstaladas):',
+	'BLOCKS_CLEANUP'			=> 'Limpar blocos',
+	'BLOCKS_CLEANUP_EXPLAIN'	=> 'Os seguintes itens foram encontrados para não existirem mais ou serem inacessíveis, e você pode, portanto, excluir todos os blocos associados a eles. Tenha em mente que alguns destes podem ser falsos positivos',
+	'BLOCKS_CLEANUP_BLOCKS'		=> 'Blocos inválidos (por exemplo, de extensões desinstaladas):',
 	'BLOCKS_CLEANUP_ROUTES'		=> 'Páginas inalcançáveis/quebradas:',
 	'BLOCKS_CLEANUP_STYLES'		=> 'Estilos desinstalados (ids):',
-	'BLOCKS_CLEANUP_SUCCESS'	=> 'Blocos purgados com sucesso',
+	'BLOCKS_CLEANUP_SUCCESS'	=> 'Blocos removidos com sucesso',
 
-	'FORUM_INDEX_SETTINGS'			=> 'Configurações de Índice do Fórum',
+	'FORUM_INDEX_SETTINGS'			=> 'Configurações do Fórum Índice',
 	'FORUM_INDEX_SETTINGS_EXPLAIN'	=> 'Estas configurações só se aplicam quando não há nenhuma página inicial definida',
 
-	'HIDE'			=> 'Ocultar',
+	'HIDE'			=> 'Esconder',
 	'HIDE_BIRTHDAY'	=> 'Ocultar seção de aniversário',
-	'HIDE_LOGIN'	=> 'Ocultar caixa de login',
-	'HIDE_ONLINE'	=> 'Esconder o Whos on-line',
+	'HIDE_LOGIN'	=> 'Ocultar caixa de entrada',
+	'HIDE_ONLINE'	=> 'Ocultar a seção Whos online',
 
-	'LAYOUT_BLOG'		=> 'Blog',
+	'LAYOUT_BLOG'		=> 'Blogue',
 	'LAYOUT_CUSTOM'		=> 'Personalizado',
-	'LAYOUT_HOLYGRAIL'	=> 'Grau Sagrado',
+	'LAYOUT_HOLYGRAIL'	=> 'Grave Sagrado',
 	'LAYOUT_PORTAL'		=> 'Portal',
-	'LAYOUT_PORTAL_ALT'	=> 'Portal (alt)',
-	'LAYOUT_SETTINGS'	=> 'Definições de layout',
+	'LAYOUT_PORTAL_ALT'	=> 'Portal (alternativo)',
+	'LAYOUT_SETTINGS'	=> 'Definições de Distribuição',
 
-	'LOG_DELETED_BLOCKS_FOR_STYLE'	=> 'Blocos de Criador de Sitemaker apagados por falta de estilo com id %s',
-	'LOG_DELETED_BLOCKS_FOR_ROUTE'	=> 'Blocos de Criador de Sites excluídos para páginas quebradas:<br />%s',
-	'LOG_DELETED_BLOCKS'			=> 'Blocos inválidos eliminados:<br />%s',
+	'LOG_DELETED_BLOCKS_FOR_STYLE'	=> 'Blocos do Sitemaker excluídos para falta de estilo com id %s',
+	'LOG_DELETED_BLOCKS_FOR_ROUTE'	=> 'Blocos do Sitemaker excluídos para páginas quebradas:<br />%s',
+	'LOG_DELETED_BLOCKS'			=> 'Blocos do Sitemaker inválidos excluídos:<br />%s',
 
 	'NAVIGATION_SETTINGS'		=> 'Configurações de navegação',
 
 	'SETTINGS_SAVED'			=> 'Suas configurações foram salvas',
-	'SHOW'						=> 'Mostrar',
-	'SHOW_FORUM_NAV'			=> 'Mostrar \'Fórum\' na barra de navegação?',
-	'SHOW_FORUM_NAV_EXPLAIN'	=> 'Quando uma página é definida como página inicial em vez do índice do fórum, devemos exibir \'Fórum\' na barra de navegação',
+	'SHOW'						=> 'Apresentar',
+	'SHOW_FORUM_NAV'			=> 'Mostrar o "Fórum" na barra de navegação?',
+	'SHOW_FORUM_NAV_EXPLAIN'	=> 'Quando uma página é definida como página inicial em vez do índice do fórum, devemos exibir o \'Fórum\' na barra de navegação',
 	'SHOW_FORUM_NAV_WITH_ICON'	=> 'Sim - com ícone:',
 ]);
