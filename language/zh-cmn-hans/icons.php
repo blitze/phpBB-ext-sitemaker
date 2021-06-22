e5108bc65304b0573dc6c64ef83b402c7082c26e
blitze <shizzah@hotmail.com>
2021-06-22 18:58:42 -0500

    New translations icons.php (Chinese Simplified) [ci skip]

---
+++ language/zh-cmn-hans/icons.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ *
+ * @package phpBB Sitemaker [English]
+ * @copyright (c) 2012 Daniel A. (blitze)
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ICON_ACCESSIBILITY'		=> '无障碍环境',
+	'ICON_ARROWS'				=> '箭头',
+	'ICON_BRAND'				=> '品牌版',
+	'ICON_CHART'				=> '图表',
+	'ICON_CURRENCY'				=> '货币',
+	'ICON_DIRECTIONAL'			=> '方向',
+	'ICON_FILE_TYPE'			=> '文件类型',
+	'ICON_FORM_CONTROL'			=> '表单控制',
+	'ICON_GENDER'				=> '两性平等',
+	'ICON_HAND'					=> '手',
+	'ICON_MEDICAL'				=> '医疗服务',
+	'ICON_PAYMENT'				=> '付款',
+	'ICON_SPINNER'				=> '旋转器',
+	'ICON_TEXT_EDITOR'			=> '文本编辑器',
+	'ICON_TRANSPORTATION'		=> '运输',
+	'ICON_VIDEO_PLAYER'			=> '视频播放器',
+	'ICON_WEB_APPLICATION'		=> 'Web 应用程序',
+
+	'ICON_COLOR'				=> '颜色',
+	'ICON_DEFAULT'				=> '默认设置',
+	'ICON_FLIP_BOTH'			=> '翻转',
+	'ICON_FLIP_HORIZONTAL'		=> '水平翻转',
+	'ICON_FLIP_VERTICAL'		=> '垂直翻转',
+	'ICON_FLOAT'				=> '浮点数',
+	'ICON_FLOAT_LEFT'			=> '左侧',
+	'ICON_FLOAT_RIGHT'			=> '右侧',
+	'ICON_FONT'					=> 'Font Icons',
+	'ICON_INSERT_UPDATE'		=> 'Insert/Update',
+	'ICON_MISC'					=> '其他',
+	'ICON_MISC_BORDERED'		=> '有边框',
+	'ICON_MISC_FIXED_WIDTH'		=> '固定宽度',
+	'ICON_MISC_PULSE'			=> '脉冲',
+	'ICON_MISC_SPINNING'		=> '连环画',
+	'ICON_ROTATION'				=> '旋转',
+	'ICON_ROTATE_90'			=> '90°',
+	'ICON_ROTATE_180'			=> '180°',
+	'ICON_ROTATE_270'			=> '270°',
+	'ICON_SIZE'					=> '大小',
+	'ICON_SIZE_LG'				=> '较大的',
+	'ICON_SIZE_SM'				=> '小的',
+	'ICON_SIZE_2X'				=> '2 x',
+	'ICON_SIZE_3X'				=> '3 x',
+	'ICON_SIZE_4X'				=> '4 x',
+	'ICON_SIZE_5X'				=> '5 x',
+
+	'NO_ICON'					=> '无图标',
+));
