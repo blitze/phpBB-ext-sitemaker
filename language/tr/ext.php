50059a5b4a18e2250bfdb3cbba8a5e893c9535fe
blitze <shizzah@hotmail.com>
2021-06-24 04:59:54 -0500

    New translations ext.php (Turkish) [ci skip]

--- language/tr/ext.php
+++ language/tr/ext.php
@@ -37,6 +37,6 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'IMAGE_DIRECTORY_NOT_WRITABLE'	=> 'The phpBB Image directory is not writable. Please change the permissions to writable by all (777 or -rwxrwxrwx within your FTP Client)',
-	'PHPBB_VERSION_UNMET'			=> 'This extension requires at least phpBB %s',
+	'IMAGE_DIRECTORY_NOT_WRITABLE'	=> 'phpBB Image dizini yazılabilir değil. Tüm izinleri yazılabilir olarak değiştirin (777 veya -rwxrwxrwx FTP Alıcısı ile)',
+	'PHPBB_VERSION_UNMET'			=> 'Bu eklenti için gereken en düşük phpBB sürümü %s',
 ));
