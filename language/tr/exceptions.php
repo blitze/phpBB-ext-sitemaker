5d120e7eea9eb4800d502c1b453593895b0f0d19
blitze <shizzah@hotmail.com>
2021-06-25 03:26:18 -0500

    New translations exceptions.php (Turkish) [ci skip]

--- language/tr/exceptions.php
+++ language/tr/exceptions.php
@@ -37,13 +37,13 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'EXCEPTION_FIELD_MISSING'		=> 'Required field missing',
-	'EXCEPTION_INVALID_ACTION'		=> 'The action does not exist',
+	'EXCEPTION_FIELD_MISSING'		=> 'Gerekli alan eksik',
+	'EXCEPTION_INVALID_ACTION'		=> 'Faliyet mevcut değil',
 	'EXCEPTION_INVALID_ARGUMENT'	=> 'Invalid argument specified for `%1$s`. Reason: %2$s',
 	'EXCEPTION_INVALID_DATA_TYPE'	=> 'The provided value is of an unexpected data type',
 	'EXCEPTION_INVALID_ENTITY'		=> 'The provided entity is of an unexpected entity class',
-	'EXCEPTION_INVALID_PROPERTY'	=> 'The requested property does not exist',
-	'EXCEPTION_OUT_OF_BOUNDS'		=> 'The requested `%1$s` does not exist',
-	'EXCEPTION_SERVICE_NOT_FOUND'	=> 'The requested service was not found',
+	'EXCEPTION_INVALID_PROPERTY'	=> 'İstenilen özellik mevcut değil',
+	'EXCEPTION_OUT_OF_BOUNDS'		=> 'İstenilen `%1$s` mevcut değil',
+	'EXCEPTION_SERVICE_NOT_FOUND'	=> 'İstenilen servis mevcut değil',
 	'EXCEPTION_UNEXPECTED_VALUE'	=> 'The requested action `%1$s` could not be performed. Reason: %2$s',
 ));
